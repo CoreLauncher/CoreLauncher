ff72d96002033ce651e8dd20cda6102c62b82ef5
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-08-08 22:24:57 +0200

    topbar script

---
+++ Frontend/static/components/topbar/index.js
@@ -0,0 +1,23 @@
+class Topbar extends HTMLElement {
+    constructor() {
+        super()
+        
+    }
+
+    connectedCallback() {
+        const Shadow = this.attachShadow({mode: 'open'});
+        this.style.height = "100px"
+        this.style.width = "100%"
+        this.style.display = "block"
+        const Iframe = document.createElement('iframe');
+        Iframe.src = '/static/components/topbar/index.html';
+        Iframe.setAttribute("frameborder", 0)
+        Iframe.style.height = "100%"
+        Iframe.style.width = "100%"
+        console.log(Iframe)
+        Shadow.appendChild(Iframe);
+    }
+}
+
+customElements.define('top-bar', Topbar)
+console.log("Loaded Topbar")
