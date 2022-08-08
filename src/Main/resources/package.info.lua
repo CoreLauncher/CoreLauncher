7365209f2182209e43767989ebab537d5d2ddaa4
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-08-08 12:59:26 +0200

    Create package.info.lua

---
+++ src/Main/resources/package.info.lua
@@ -0,0 +1,35 @@
+-- See https://github.com/Dot-lua/TypeWriter/wiki/package.info.lua-format for more info
+
+return { InfoVersion = 1, -- Dont touch this
+
+    ID = "CoreLauncher", -- A unique id 
+    Name = "CoreLauncher",
+    Description = "CoreLauncher",
+    Version = "1.0.0",
+
+    Author = {
+        Developers = {
+            "CoreByte"
+        },
+        Contributors = {}
+    },
+
+    Dependencies = {
+        Luvit = {
+            "creationix/coro-http"
+        },
+        Git = {},
+        Dua = {}
+    },
+
+    Contact = {
+        Website = "https://cubic-inc.ga",
+        Source = "https://github.com/Dot-lua/TypeWriter/",
+        Socials = {}
+    },
+
+    Entrypoints = {
+        Main = "ga.CoreLauncher"
+    }
+
+}
