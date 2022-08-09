231f496cdbb8a2b83d03e1ea710abfa2fe437b0a
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-08-09 19:11:00 +0200

    minecraft game

---
+++ src/Main/lua/ga/CoreLauncher/Games/Minecraft/Main.lua
@@ -0,0 +1,16 @@
+return {
+    Info = {
+        Name = "Minecraft",
+        Description = "Minecraft is a sandbox video game created by Swedish game developer Markus Persson and later developed by Mojang.",
+        Icon = "https://www.minecraft.net/etc.clientlibs/minecraft/clientlibs/main/resources/img/minecraft-creeper-face.jpg",
+        Developer = {
+            Name = "Mojang",
+            Website = "https://www.minecraft.net/en-us/"
+        }
+    },
+    Functions = {
+
+    }
+    
+
+}
