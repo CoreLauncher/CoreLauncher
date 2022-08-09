dd8756a4f2ed23f94fe34e808a07ac7b6f4defea
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-08-09 22:37:08 +0200

    get game info

--- Frontend/instances/index.js
+++ Frontend/instances/index.js
@@ -1,6 +1,14 @@
 window.addEventListener(
     "load",
     async function() {
-        
+        const urlSearchParams = new URLSearchParams(window.location.search); const QueryParameters = Object.fromEntries(urlSearchParams.entries());
+        const GameId = QueryParameters["game"];
+        console.log(GameId)
+        const Game = await CoreLauncher.IPC.Send(
+            "Main",
+            "GetGame",
+            GameId
+        )
+        console.log(Game)
     }
 )
