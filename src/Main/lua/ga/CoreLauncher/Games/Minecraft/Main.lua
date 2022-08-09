80caecf1c16e2f310957e6436d47ecb98a65cda1
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-08-09 21:45:43 +0200

    add gameid

--- src/Main/lua/ga/CoreLauncher/Games/Minecraft/Main.lua
+++ src/Main/lua/ga/CoreLauncher/Games/Minecraft/Main.lua
@@ -1,5 +1,6 @@
 return {
     Info = {
+        Id = "MinecraftJava",
         Name = "Minecraft",
         Description = "Minecraft is a sandbox video game created by Swedish game developer Markus Persson and later developed by Mojang.",
         Icon = "https://www.minecraft.net/etc.clientlibs/minecraft/clientlibs/main/resources/img/minecraft-creeper-face.jpg",
