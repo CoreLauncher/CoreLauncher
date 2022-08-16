c50d74c0dfc331d2806de7043be2e3427dbd2e34
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-08-16 12:37:10 +0200

    add account callbacks

---
+++ src/Main/lua/ga/CoreLauncher/Modules/Accounts/Main.lua
@@ -0,0 +1,25 @@
+CoreLauncher.IPC:RegisterMessage(
+    "AccountConnected",
+    function(Name)
+        p(Name)
+        return CoreLauncher.Accounts:AccountConnected(Name)
+    end
+)
+
+CoreLauncher.IPC:RegisterMessage(
+    "ConnectAccount",
+    function (Name)
+        return CoreLauncher.Accounts:Connect(Name)
+    end
+)
+
+CoreLauncher.IPC:RegisterMessage(
+    "AccountCallback",
+    function (Data)
+        CoreLauncher.Accounts:AccountCallback(Data.Type, Data.Data)
+        local Window = CoreLauncher.Window
+        Window:Focus()
+        Window:setAlwaysOnTop(true)
+        Window:setAlwaysOnTop(false)
+    end
+)
