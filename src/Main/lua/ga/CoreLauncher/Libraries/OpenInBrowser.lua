639bd24cfb7d314b62bba6da58c7526b10552b16
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-08-16 12:35:25 +0200

    Create OpenInBrowser.lua

---
+++ src/Main/lua/ga/CoreLauncher/Libraries/OpenInBrowser.lua
@@ -0,0 +1,9 @@
+local Spawn = require("coro-spawn")
+local Base = require("base64")
+return function (Link)
+    if TypeWriter.Os == "win32" then
+        os.execute("start http://localhost/redirect#" .. Base.encode(Link))
+    else
+        os.execute("open " .. Link)
+    end
+end
