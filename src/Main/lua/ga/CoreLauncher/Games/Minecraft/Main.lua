5ae50a70a208f24e60924ac4e0edfb0a0523c599
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-09-04 22:16:20 +0200

    change icon

--- src/Main/lua/ga/CoreLauncher/Games/Minecraft/Main.lua
+++ src/Main/lua/ga/CoreLauncher/Games/Minecraft/Main.lua
@@ -3,7 +3,7 @@ return {
         Id = "MinecraftJava",
         Name = "Minecraft",
         Description = "Minecraft is a sandbox video game created by Swedish game developer Markus Persson and later developed by Mojang.",
-        Icon = "https://www.minecraft.net/etc.clientlibs/minecraft/clientlibs/main/resources/img/minecraft-creeper-face.jpg",
+        Icon = "https://lh3.googleusercontent.com/R74PW5uRF3klb1oz6OSsjEO-Wbhc_tISRrdjRrVtLWRbdMfOr8gd-nHBLF-4c4HifLNWl6gz2N8zk4jtb0KO=s400",
         Developer = {
             Name = "Mojang",
             Website = "https://www.minecraft.net/en-us/"
