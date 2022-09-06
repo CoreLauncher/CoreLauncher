f274280540866224d0966b3a900fb82ada255159
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-09-06 13:00:07 +0200

    backup old game page

---
+++ Frontend/oldoldgames/index.js
@@ -0,0 +1,26 @@
+window.addEventListener(
+    "load",
+    async function() {
+        const Template = document.getElementsByClassName("game-tile")[0]
+        Template.remove()
+        const GameHolder = document.getElementById("games-holder")
+        const Games = await CoreLauncher.IPC.Send(
+            "Main",
+            "GetGames"
+        )
+        Games.forEach(Game => {
+            const GameElement = Template.cloneNode(true)
+            GameElement.querySelector("#game-icon").src = Game.Icon
+            GameElement.querySelector("#game-name").innerText = Game.Name
+            GameElement.querySelector("#game-creator").innerText = Game.Developer.Name
+            GameElement.querySelector("#game-creator").setAttribute("href", Game.Developer.Website)
+            GameElement.addEventListener(
+                "click",
+                function() {
+                    location = "/instances/?game=" + Game.Id
+                }
+            )
+            GameHolder.appendChild(GameElement)
+        });
+    }
+)
