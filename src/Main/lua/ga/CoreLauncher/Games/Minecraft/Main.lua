4287bb5dae00fe6dff3de4ad05b295004477dca5
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-09-07 08:32:36 +0200

    long

--- src/Main/lua/ga/CoreLauncher/Games/Minecraft/Main.lua
+++ src/Main/lua/ga/CoreLauncher/Games/Minecraft/Main.lua
@@ -2,6 +2,7 @@ return {
     Info = {
         Id = "MinecraftJava",
         Name = "Minecraft",
+        LongName = "Minecraft: Java edition",
         Description = "Minecraft is a sandbox video game created by Swedish game developer Markus Persson and later developed by Mojang.",
         Icon = "https://lh3.googleusercontent.com/R74PW5uRF3klb1oz6OSsjEO-Wbhc_tISRrdjRrVtLWRbdMfOr8gd-nHBLF-4c4HifLNWl6gz2N8zk4jtb0KO=s400",
         Developer = {
