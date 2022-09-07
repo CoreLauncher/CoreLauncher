d6eb9e03fc40aa457164f4f2d609ea1471a0ed68
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-09-07 16:35:55 +0200

    add banner to game info

--- src/Main/lua/ga/CoreLauncher/Games/Minecraft/Main.lua
+++ src/Main/lua/ga/CoreLauncher/Games/Minecraft/Main.lua
@@ -5,6 +5,7 @@ return {
         LongName = "Minecraft: Java edition",
         Description = "Minecraft is a sandbox video game created by Swedish game developer Markus Persson and later developed by Mojang.",
         Icon = "https://lh3.googleusercontent.com/R74PW5uRF3klb1oz6OSsjEO-Wbhc_tISRrdjRrVtLWRbdMfOr8gd-nHBLF-4c4HifLNWl6gz2N8zk4jtb0KO=s400",
+        Banner = "https://www.minecraft.net/content/dam/games/minecraft/key-art/Xbox_Minecraft_WildUpdate_Main_.Net_1170x500.jpg",
         Developer = {
             Name = "Mojang",
             Website = "https://www.minecraft.net/en-us/"
