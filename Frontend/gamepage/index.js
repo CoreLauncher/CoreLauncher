6c6388315e74bf6175f4895b5378a0e154b768e7
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-09-07 08:39:56 +0200

    change one element

--- Frontend/gamepage/index.js
+++ Frontend/gamepage/index.js
@@ -1,6 +1,14 @@
 window.addEventListener(
     "load",
     async function() {
-        
+        const urlSearchParams = new URLSearchParams(window.location.search); const QueryParameters = Object.fromEntries(urlSearchParams.entries());
+        const GameId = QueryParameters["game"];
+        const Game = await CoreLauncher.IPC.Send(
+            "Main",
+            "GetGame",
+            GameId
+        )
+        console.log(Game)
+        document.getElementById("gametitle").innerText = Game.LongName
     }
 )
