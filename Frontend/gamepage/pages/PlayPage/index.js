1b082087b55e9c8239bfb2ebf763d2305a9146cb
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-09-07 16:36:43 +0200

    load banner

--- Frontend/gamepage/pages/PlayPage/index.js
+++ Frontend/gamepage/pages/PlayPage/index.js
@@ -1,6 +1,12 @@
 window.addEventListener(
     "load",
     async function() {
-        
+        const Game = await CoreLauncher.IPC.Send(
+            "Main",
+            "GetGame",
+            QueryParameters.game
+        )
+        console.log(Game)
+        document.getElementById("bannerimage").src = Game.Banner
     }
 )
