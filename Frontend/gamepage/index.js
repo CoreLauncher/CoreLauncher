4a5568f0f9c657adf926a246e439d4e24ac0dee3
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-09-07 15:04:22 +0200

    sort of working...?

--- Frontend/gamepage/index.js
+++ Frontend/gamepage/index.js
@@ -11,5 +11,22 @@ window.addEventListener(
         console.log(Game)
         document.getElementById("gametitle").innerText = Game.LongName
         document.getElementById("developername").innerText = `By ${Game.Developer.Name}`
+
+        const Template = document.querySelector("#tabbutton")
+        console.log(Template)
+        Template.remove()
+        const TabHolder = document.getElementById("tabholder")
+        const Tabs = Game.Pages
+        Tabs.forEach(TabName => {
+            const TabElement = Template.cloneNode(true)
+            console.log(TabName)
+            const PageMeta = await (
+                await fetch(`/gamepage/pages/${TabName}/Meta.json`)
+            ).json()
+            console.log(PageMeta)
+            TabElement.querySelector("#buttonlabel").innerText = PageMeta.Name
+            console.log(TabName)
+            TabHolder.appendChild(TabElement)
+        });
     }
 )
