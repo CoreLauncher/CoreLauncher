c89911b225d44ac1c7e24f3f2e7e56543bd2f5e7
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-09-07 15:49:01 +0200

    put Queryparameters in global scope

--- Frontend/gamepage/index.js
+++ Frontend/gamepage/index.js
@@ -1,7 +1,6 @@
 window.addEventListener(
     "load",
     async function() {
-        const urlSearchParams = new URLSearchParams(window.location.search); const QueryParameters = Object.fromEntries(urlSearchParams.entries());
         const GameId = QueryParameters["game"];
         const Game = await CoreLauncher.IPC.Send(
             "Main",
--- Frontend/static/script/index.js
+++ Frontend/static/script/index.js
@@ -1,4 +1,5 @@
 console.log("Loading Core")
+const urlSearchParams = new URLSearchParams(window.location.search); window.QueryParameters = Object.fromEntries(urlSearchParams.entries());
 window.CoreLauncher = {
     IPC: new OpenIPC("CoreLauncher", (document.currentScript || {getAttribute: function() {return undefined}}).getAttribute("IPC") || "Render"),
 }
