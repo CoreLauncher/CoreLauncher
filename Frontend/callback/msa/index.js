f54aefcb76d12ed9fa2387edd92671040a9b732e
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-09-08 13:57:04 +0200

    msa page wip

---
+++ Frontend/callback/msa/index.js
@@ -0,0 +1,35 @@
+function GetHashQuery() {
+    const Data = {}
+    location.hash.split("#")[1].split("&").forEach(
+        function(Line) {
+            var LineData = Line.split("=")
+            var Key = LineData[0]
+            LineData.shift()
+            var Value = Number(LineData[0]) || LineData.join("=")
+            Data[Key] = Value
+        }
+    )
+    return Data
+}
+
+window.addEventListener(
+    "load",
+    async function() {
+        const HashQuery = GetHashQuery()
+        if (HashQuery.close == "true") {
+            document.getElementById("text").innerText = "You can now close this window!"
+        } else {
+            await CoreLauncher.IPC.Send(
+                "Main",
+                "AccountCallback",
+                {
+                    Type: "Discord",
+                    Data: HashQuery
+                }
+            )
+            //this.location.hash = "close=true"
+            //this.location.reload()
+        }
+        
+    }
+)
