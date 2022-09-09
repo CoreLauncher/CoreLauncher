e262a3356aa3abd3c3882308c68e4ceebed21d6a
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-09-09 19:03:56 +0200

    remove weird caching

--- src/Main/lua/ga/CoreLauncher/Libraries/Discord.lua
+++ src/Main/lua/ga/CoreLauncher/Libraries/Discord.lua
@@ -4,41 +4,21 @@ local Json = require("json")
 local Request = require("coro-http").request
 local TablePatcher = Import("ga.CoreLauncher.Libraries.TablePatcher")
 
-function Discord.GetUser(Override)
-    local function GetEpoch()
-        return os.time()
+function Discord.GetUser()
+    local Token = CoreLauncher.Config:GetKey("Accounts.Discord.AccessToken.access_token")
+    if Token == nil then
+        return nil
     end
-    local function SetUserData(UserData)
-        UserData.CachedAt = GetEpoch()
-        CoreLauncher.Config:SetKey(
-            "Accounts.Discord.UserData",
-            UserData
-        )
-    end
-    local function Get(Key)
-        local Response, Body = Request(
-            "GET",
-            "https://discordapp.com/api/users/@me",
-            {
-                {"Authorization", "Bearer " .. Override},
-                {"User-Agent", "CoreLauncher"},
-                {"Content-Type", "application/json"}
-            }
-        )
-        return TablePatcher(Json.decode(Body))
-    end
-    if type(Override) == "string" then
-        local UserData = Get(Override)
-        SetUserData(UserData)
-        return UserData
-    else
-        local UserData = Get(CoreLauncher.Config:GetKey("Accounts.Discord.TokenData.access_token"))
-        SetUserData(UserData)
-        return UserData
-    end
-    local CachedData = CoreLauncher.Config:GetKey("Accounts.Discord.UserData")
-    p(CachedData)
-
+    local Response, Body = Request(
+        "GET",
+        "https://discordapp.com/api/users/@me",
+        {
+            {"Authorization", "Bearer " .. Token},
+            {"User-Agent", "CoreLauncher"},
+            {"Content-Type", "application/json"}
+        }
+    )
+    return TablePatcher(Json.decode(Body))
 end
 
 return Discord
