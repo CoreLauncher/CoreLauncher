fcfdf49a0e7503c469e27443432a20f091c98007
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-09-10 15:59:09 +0200

    rework page system

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,3 +1,34 @@
+const Pages = []
+var SelectedPage = null
+async function RegisterPage(ClickBlock, IndicatorBlock, IndicatorClass, PageUrl, PageName, Selected = false) {
+    const Data = {
+        ClickBlock: ClickBlock,
+        IndicatorBlock: IndicatorBlock,
+        IndicatorClass: IndicatorClass,
+        PageUrl: PageUrl,
+        PageName: PageName
+    }
+    Pages.push(Data)
+    ClickBlock.addEventListener(
+        "click",
+        function() {
+            if (SelectedPage == Data) {console.log("Page already selected"); return}
+            console.log(Pages)
+            for (const Page of Pages) {
+                console.log(Page)
+                Page.IndicatorBlock.classList.remove(Page.IndicatorClass)
+            }
+            Data.IndicatorBlock.classList.add(Data.IndicatorClass)
+            document.getElementById("mainframe").src = Data.PageUrl
+            SelectedPage = Data
+        }
+    )
+    if (Selected == true) {
+        ClickBlock.click()
+    }
+    console.log(Data)
+}
+
 async function LoadGameCards() {
     const Template = document.querySelector("#game-card")
     console.log(Template)
@@ -11,12 +42,7 @@ async function LoadGameCards() {
         const GameElement = Template.cloneNode(true)
         GameElement.querySelector("#game-icon").src = Game.Icon
         GameElement.querySelector("#game-label").innerText = Game.Name
-        GameElement.addEventListener(
-            "click",
-            function() {
-                document.getElementById("mainframe").src = "/gamepage/?game=" + Game.Id
-            }
-        )
+        RegisterPage(GameElement, GameElement.querySelector("#selectedblock"), "selected-block", `/gamepage/?game=${Game.Id}`, Game.Name)
         GameHolder.appendChild(GameElement)
     });
 }
@@ -47,5 +73,6 @@ window.addEventListener(
         await LoadGameCards()
         await CheckDiscord()
         await LoadUI()
+        RegisterPage(document.getElementById("logo-holder"), document.getElementById("logo-holder"), "logo-selected", "/about/", "About", true)
     }
 )
