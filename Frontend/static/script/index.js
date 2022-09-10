3ade983801cde8e4bfe4d1c3023e5be0b828948c
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-09-10 21:37:18 +0200

    new function for string

--- Frontend/static/script/index.js
+++ Frontend/static/script/index.js
@@ -12,3 +12,9 @@ CoreLauncher.ExtLink = async function(Link) {
 }
 
 console.log("Core Loaded!")
+
+String.prototype.interpolate = function(params) {
+    const names = Object.keys(params);
+    const vals = Object.values(params);
+    return new Function(...names, `return \`${this}\`;`)(...vals);
+}
