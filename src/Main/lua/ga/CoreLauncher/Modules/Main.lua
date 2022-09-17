2c72c57f0708994573677926db9f5feaff26eb82
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-09-17 16:20:46 +0200

    rename

--- src/Main/lua/ga/CoreLauncher/Modules/Main.lua
+++ src/Main/lua/ga/CoreLauncher/Modules/Main.lua
@@ -1,4 +1,4 @@
 Import("ga.CoreLauncher.Modules.Accounts")
-Import("ga.CoreLauncher.Modules.GetGames")
+Import("ga.CoreLauncher.Modules.Games")
 Import("ga.CoreLauncher.Modules.Other")
 Import("ga.CoreLauncher.Modules.Package")
