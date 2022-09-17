f34f4f8a859102ca9ed021f6abf1cd877b08abe0
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-09-17 18:38:44 +0200

    add instance name to every instance

--- src/Main/lua/ga/CoreLauncher/Modules/Games/Instances/Main.lua
+++ src/Main/lua/ga/CoreLauncher/Modules/Games/Instances/Main.lua
@@ -1,6 +1,13 @@
 CoreLauncher.IPC:RegisterMessage(
     "Games.Instances.GetInstanceProperties",
     function(Game)
-        return CoreLauncher.Games[Game].Functions.GetInstanceProperties()
+        local Properties = CoreLauncher.Games[Game].Functions.GetInstanceProperties()
+        Properties.InstanceName = {
+            Label = "Instance Name",
+            Type = "string",
+            Default = "Instance",
+            Index = -1
+        }
+        return Properties
     end
 )
