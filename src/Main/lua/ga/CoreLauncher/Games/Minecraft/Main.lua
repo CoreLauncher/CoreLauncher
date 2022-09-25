7064910b97042b6e5f267d31a6c2198cead1f03f
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-09-25 21:14:30 +0200

    add functions to minecraft

--- src/Main/lua/ga/CoreLauncher/Games/Minecraft/Main.lua
+++ src/Main/lua/ga/CoreLauncher/Games/Minecraft/Main.lua
@@ -1,6 +1,31 @@
 local Data = {}
-Data.Cache = {}
 
+local QueryEncode = require("querystring").stringify
+local ModrinthBaseUrl = string.format(
+    "https://api.modrinth.com/%s/",
+    "v2"
+)
+
+local function GenerateModrinthFacets(Facets)
+    local NewData = {}
+    for _, FacetBlock in pairs(Facets) do
+        local NewFacetBlock = {}
+        for _, Facet in pairs(FacetBlock) do
+            table.insert(
+                NewFacetBlock,
+                string.format(
+                    "%s:%s",
+                    Facet[1],
+                    Facet[2]
+                )
+            )
+        end
+        table.insert(NewData, NewFacetBlock)
+    end
+    return require("json").encode(NewData)
+end
+
+Data.Cache = {}
 Data.Cache.VersionManifest = ({CoreLauncher.Http.JsonRequest("GET", "https://piston-meta.mojang.com/mc/game/version_manifest_v2.json")})[2]
 
 Data.Info = {
@@ -38,7 +63,7 @@ Data.Functions = {
                 Label = "Minecraft Version",
                 Type = "table",
                 Values = Versions,
-                Default = Versions[1],
+                Default = Data.Cache.VersionManifest.latest.release,
                 Index = 1
             }
         end
@@ -72,18 +97,101 @@ Data.Functions = {
         end
         return Properties
     end,
-    GetInstanceComment = function (InstanceProperties)
-        
+    GetInstanceComment = function (Instance)
+        local Comment = Instance.Properties.ModType .. " " .. Instance.Properties.Version
+        if Instance.Editable == false then
+            Comment = Comment .. " (Not editable)"
+        end
+        return Comment
     end,
     GetDefaultInstances = function ()
         return {
             {
                 Id = "LatestRelease",
+                Name = "Latest Release",
                 Removable = false,
-                Properties = {}
+                Editable = false,
+                Properties = {
+                    Version = Data.Cache.VersionManifest.latest.release,
+                    ModType = "Vanilla",
+                    Memory = 4
+                }
             }
         }
-    end
+    end,
+    ModSources = {
+        Modrinth = {
+            GetSearchProperties = function ()
+                local SearchProperties = {}
+            end,
+            Search = function (Query, Properties, Instance, Page)
+                p(Instance)
+                local Facets = {
+                    {
+                        {"project_type", "mod"}
+                    },
+                    {
+                        {"versions", Instance.Properties.Version}
+                    }
+                }
+                local ModType = Instance.Properties.ModType
+                local LoaderFacet = {
+                    {"categories", ModType:lower()}
+                }
+                if ModType == "Quilt" then
+                    table.insert(
+                        LoaderFacet,
+                        {"categories", "fabric"}
+                    )
+                end
+                table.insert(Facets, LoaderFacet)
+                local Response, Data = CoreLauncher.Http.JsonRequest(
+                    "GET",
+                    ModrinthBaseUrl .. "search?" .. QueryEncode(
+                        {
+                            query = Query,
+                            limit = 20,
+                            facets = GenerateModrinthFacets(Facets),
+                            offset = (Page - 1) * 20
+                        }
+                    )
+                )
+                local ReturnData = {
+                    HitCount = #Data.hits,
+                    TotalHitCount = Data.total_hits,
+                    Limit = Data.limit,
+                    Offset = Data.offset,
+                    PageCount = math.ceil(Data.total_hits / 20),
+                    Hits = {}
+                }
+                for Index, Hit in pairs(Data.hits) do
+                    local Categories = {}
+                    for Index, Category in pairs(Hit.categories) do
+                        table.insert(Categories, Category:sub(1, 1):upper() .. Category:sub(2))
+                    end
+                    table.insert(
+                        ReturnData.Hits,
+                        {
+                            Source = "Modrinth",
+                            Id = Hit.project_id,
+                            Slug = Hit.slug,
+                            Name = Hit.title,
+                            Author = Hit.author,
+                            Description = Hit.description,
+                            Icon = Hit.icon_url,
+                            Licence = Hit.licence,
+                            Link = "https://modrinth.com/" .. Hit.project_type .. "/" .. Hit.slug,
+                            Envoirments = {
+                                Client = Hit.client_side,
+                                Server = Hit.server_side
+                            },
+                            Categories = Categories
+                        }
+                    )
+                end
+                return ReturnData
+            end
+        }
+    }
 }
-Data.Functions.GetInstanceProperties()
 return Data
