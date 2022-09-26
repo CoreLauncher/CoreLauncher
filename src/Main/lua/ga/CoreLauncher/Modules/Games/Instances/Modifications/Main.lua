a10207147378350636f8fcf462b888c1678f4ff3
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-09-26 22:14:08 +0200

    add new remote functions for mods

--- src/Main/lua/ga/CoreLauncher/Modules/Games/Instances/Modifications/Main.lua
+++ src/Main/lua/ga/CoreLauncher/Modules/Games/Instances/Modifications/Main.lua
@@ -25,16 +25,56 @@ CoreLauncher.IPC:RegisterMessage(
 CoreLauncher.IPC:RegisterMessage(
     "Games.Instances.Modifications.AddToInstance",
     function(Data)
-        p(Data)
         local Game = Data.Game
         local InstanceId = Data.InstanceId
         local ModData = Data.ModData
-        local Instance
-        for Index, InstanceL in pairs(CoreLauncher.Config:GetKey(string.format("Games.%s.Instances", Game))) do
-            if InstanceL.Id == InstanceId then
-                Instance = InstanceL
-                break
-            end
+        local Instance = CoreLauncher.Config:GetKey(
+            string.format(
+                "Games.%s.Instances.%s",
+                Game,
+                InstanceId
+            )
+        )
+        if ModData.Version == "latest" then
+            ModData.Version = CoreLauncher.Games[Game].Functions.ModSources[ModData.Source].GetLatestModVersion(Instance, ModData.Id).id
         end
+        CoreLauncher.Config:SetKey(
+            string.format(
+                "Games.%s.Instances.%s.Modifications.Enabled.%s",
+                Game,
+                InstanceId,
+                ModData.Id
+            ),
+            ModData
+        )
+    end
+)
+
+CoreLauncher.IPC:RegisterMessage(
+    "Games.Instances.Modifications.ListMods",
+    function(Data)
+        local Game = Data.Game
+        local InstanceId = Data.InstanceId
+        local Instance = CoreLauncher.Config:GetKey(
+            string.format(
+                "Games.%s.Instances.%s",
+                Game,
+                InstanceId
+            )
+        )
+        local function GetFor(ModList)
+            local Return = {}
+            for Index, Mod in pairs(ModList) do
+                Mod = table.deepcopy(Mod)
+                Mod.Versions = CoreLauncher.Games[Game].Functions.ModSources[Mod.Source].GetVersionsSupportedForInstance(Instance, Mod.Id)
+                table.insert(Return, Mod)
+            end
+            return Return
+        end
+        local Mods = {
+            Enabled = GetFor(Instance.Modifications.Enabled),
+            Disabled = GetFor(Instance.Modifications.Disabled)
+        }
+        return Mods
     end
 )
