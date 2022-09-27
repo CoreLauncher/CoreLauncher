a8a6e7ffeb15e844da38f0896f28798223026be2
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-09-27 15:50:07 +0200

    work on game launching

--- src/Main/lua/ga/CoreLauncher/Modules/Games/Main.lua
+++ src/Main/lua/ga/CoreLauncher/Modules/Games/Main.lua
@@ -20,3 +20,20 @@ CoreLauncher.IPC:RegisterMessage(
         return CoreLauncher.Games[GameId].Info
     end
 )
+
+CoreLauncher.IPC:RegisterMessage(
+    "Games.LaunchGame",
+    function(Data)
+        local Game = Data.Game
+        local InstanceId = Data.InstanceId
+        local Instance = CoreLauncher.Config:GetKey(
+            string.format(
+                "Games.%s.Instances.%s",
+                Game,
+                InstanceId
+            )
+        )
+        p(Instance)
+        CoreLauncher.Games[Game].Functions.LaunchGame(Instance)
+    end
+)
