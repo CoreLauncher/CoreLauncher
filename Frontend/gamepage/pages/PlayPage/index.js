0432c98d5a79d6e7ce82a67cdf37de0141155006
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-09-27 15:50:28 +0200

    work on play button

--- Frontend/gamepage/pages/PlayPage/index.js
+++ Frontend/gamepage/pages/PlayPage/index.js
@@ -7,5 +7,37 @@ window.addEventListener(
             QueryParameters.game
         )
         document.getElementById("bannerimage").src = Game.Banner
+
+        const Instances = await CoreLauncher.IPC.Send(
+            "Main",
+            "Games.Instances.GetInstances", 
+            Game.Id
+        )
+        p(Instances)
+        const CurrentInstance = document.getElementById("currentinstance")
+        for (const InstanceID in Instances) {
+            const Instance = Instances[InstanceID]
+
+            const Option = document.createElement("option")
+            Option.value = Instance.Id 
+            Option.innerText = `${Instance.Name} (${Instance.Comment})`
+            CurrentInstance.appendChild(Option)
+        }
+
+        const PlayButton = document.getElementById("playbutton")
+        PlayButton.addEventListener(
+            "click",
+            async function() {
+                p(CurrentInstance.value)
+                const ReturnData = await CoreLauncher.IPC.Send(
+                    "Main",
+                    "Games.LaunchGame", 
+                    {
+                        Game: Game.Id,
+                        InstanceId: CurrentInstance.value
+                    }
+                )
+            }
+        )
     }
 )
