9fa19eab5e8dc2f0c15ac156d1716b8a626318a6
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-09-28 22:14:08 +0200

    work on progressbar

---
+++ src/Main/lua/ga/CoreLauncher/Libraries/ProgessBar.lua
@@ -0,0 +1,15 @@
+local ProgressBar = Class:extend()
+
+function ProgressBar:initialize()
+    self.Stage = "Unknown"
+end
+
+function ProgressBar:SetStage(Name)
+    
+end
+
+function ProgressBar:SetProgress(Count, Total)
+    
+end
+
+return ProgressBar
