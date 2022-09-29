8bd12c2925386a73a035901ca11c4f95d1a90a4f
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-09-29 16:57:35 +0200

    finish play page

--- Frontend/gamepage/pages/PlayPage/index.js
+++ Frontend/gamepage/pages/PlayPage/index.js
@@ -1,48 +1,86 @@
-window.addEventListener(
-    "load",
-    async function() {
-        const PlayBar = document.getElementById("playbar")
-        const BarFiller = document.getElementById("barfiller")
-        const StageText = document.getElementById("stagetext")
-        const ProgressText = document.getElementById("progresstext")
-        CoreLauncher.IPC.RegisterMessage(
-            "ProgressBar.Update",
-            async function(Data) {
-                p(Data)
-                BarFiller.style.width = `${Data.Percent}%`
-                StageText.innerText = Data.Stage
-                ProgressText.innerText = `${Data.Count}/${Data.Total} (${Math.floor(Data.Percent)}%)`
-            }
-        )
+var Game
+var Instances 
+var PlayButton
 
-        const Game = await CoreLauncher.IPC.Send(
+var CanClick = true
+var ButtonKillsGame = false
+var UpdateButton = true
+
+async function GetGameState() {
+    if (UpdateButton == true) {
+        const State = await CoreLauncher.IPC.Send(
             "Main",
-            "Games.GetGame",
-            QueryParameters.game
+            "Games.GetState"
         )
-        document.getElementById("bannerimage").src = Game.Banner
-
-        const Instances = await CoreLauncher.IPC.Send(
-            "Main",
-            "Games.Instances.GetInstances", 
-            Game.Id
-        )
-        p(Instances)
-        const CurrentInstance = document.getElementById("currentinstance")
-        for (const InstanceID in Instances) {
-            const Instance = Instances[InstanceID]
-
-            const Option = document.createElement("option")
-            Option.value = Instance.Id 
-            Option.innerText = `${Instance.Name} (${Instance.Comment})`
-            CurrentInstance.appendChild(Option)
+        if (State.Running) {
+            PlayButton.classList.remove("button-positive")
+            PlayButton.classList.add("button-danger")
+            p("Running")
+            PlayButton.innerText = "Kill Game"
+        } else {
+            PlayButton.classList.add("button-positive")
+            PlayButton.classList.remove("button-danger")
+            PlayButton.innerText = "Play"
         }
+        ButtonKillsGame = State.Running
+    }
+}
 
-        const PlayButton = document.getElementById("playbutton")
-        PlayButton.addEventListener(
-            "click",
-            async function() {
-                p(CurrentInstance.value)
+async function LoadProgressBar() {
+    const PlayBar = document.getElementById("playbar")
+    const BarFiller = document.getElementById("barfiller")
+    const StageText = document.getElementById("stagetext")
+    const ProgressText = document.getElementById("progresstext")
+    CoreLauncher.IPC.RegisterMessage(
+        "ProgressBar.Update",
+        async function(Data) {
+            p(Data)
+            BarFiller.style.width = `${Data.Percent}%`
+            StageText.innerText = Data.Stage
+            ProgressText.innerText = `${Data.Count}/${Data.Total} (${Math.floor(Data.Percent)}%)`
+            if (Data.Shown == true) {
+                PlayBar.classList.add("loadingbarvisible")
+            } else {
+                PlayBar.classList.remove("loadingbarvisible")
+            }
+        }
+    )
+}
+
+async function LoadInstanceDropdown() {
+    p(Instances)
+    const CurrentInstance = document.getElementById("currentinstance")
+    for (const InstanceID in Instances) {
+        const Instance = Instances[InstanceID]
+
+        const Option = document.createElement("option")
+        Option.value = Instance.Id 
+        Option.innerText = `${Instance.Name} (${Instance.Comment})`
+        CurrentInstance.appendChild(Option)
+    }
+}
+
+async function LoadPlayButton() {
+    await GetGameState()
+    const CurrentInstance = document.getElementById("currentinstance")
+    PlayButton.addEventListener(
+        "click",
+        async function() {
+            if (CanClick == false) {
+                return
+            }
+            if (ButtonKillsGame == true) {
+                const ReturnData = await CoreLauncher.IPC.Send(
+                    "Main",
+                    "Games.KillGame", 
+                    {
+                        Game: Game.Id,
+                        InstanceId: CurrentInstance.value
+                    }
+                )
+            } else {
+                CanClick = false
+                UpdateButton = false
                 const ReturnData = await CoreLauncher.IPC.Send(
                     "Main",
                     "Games.LaunchGame", 
@@ -51,8 +89,37 @@ window.addEventListener(
                         InstanceId: CurrentInstance.value
                     }
                 )
-                p("Sent")
+                await GetGameState()
+                UpdateButton = true
+                CanClick = true
             }
+        }
+    )
+}
+
+
+window.addEventListener(
+    "load",
+    async function() {
+        Game = await CoreLauncher.IPC.Send(
+            "Main",
+            "Games.GetGame",
+            QueryParameters.game
         )
+        Instances = await CoreLauncher.IPC.Send(
+            "Main",
+            "Games.Instances.GetInstances", 
+            Game.Id
+        )
+        PlayButton = document.getElementById("playbutton")
+        await LoadProgressBar()
+        document.getElementById("bannerimage").src = Game.Banner
+        await LoadInstanceDropdown()
+        await LoadPlayButton()
+
+        while (true) {
+            await Sleep(1000)
+            const GameState = await GetGameState()
+        }
     }
 )
