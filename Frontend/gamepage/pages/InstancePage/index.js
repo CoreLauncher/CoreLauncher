dff39894047c4609f7d3fc18d0c168f6ad5e6e01
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-10-08 15:07:27 +0200

    Update index.js

--- Frontend/gamepage/pages/InstancePage/index.js
+++ Frontend/gamepage/pages/InstancePage/index.js
@@ -337,6 +337,10 @@ async function LoadSearchBar() {
             OnChange()
         }
     )
+    document.getElementById("modsource").addEventListener(
+        "change",
+        OnChange
+    )
 
     OnInstanceChange = OnChange
 
