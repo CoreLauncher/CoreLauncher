71a2f877bae5be9a15e566c0d234670a5ea5233e
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-10-13 22:13:36 +0200

    Create index.js

---
+++ Frontend/assets/script/index.js
@@ -0,0 +1,26 @@
+const urlSearchParams = new URLSearchParams(window.location.search); window.QueryParameters = Object.fromEntries(urlSearchParams.entries());
+window.CoreLauncher = {
+    IPC: new OpenIPC("CoreLauncher", (document.currentScript || {getAttribute: function() {return undefined}}).getAttribute("IPC") || "Render"),
+}
+window.p = console.log
+CoreLauncher.ExtLink = async function(Link) {
+    await CoreLauncher.IPC.Send(
+        "Main",
+        "Other.ExtLink",
+        Link
+    )
+}
+
+String.prototype.interpolate = function(params) {
+    const names = Object.keys(params);
+    const vals = Object.values(params);
+    return new Function(...names, `return \`${this}\`;`)(...vals);
+}
+
+Number.prototype.clamp = function(min, max) {
+    return Math.min(Math.max(this, min), max);
+};
+
+function Sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
