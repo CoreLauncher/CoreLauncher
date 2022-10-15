356a46dac0116a5fd10f7c0d13a155a057b749a7
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-10-15 21:55:56 +0200

    bugfix

--- src/Main/resources/package.info.lua
+++ src/Main/resources/package.info.lua
@@ -5,7 +5,7 @@ return { InfoVersion = 1, -- Dont touch this
     ID = "CoreLauncher", -- A unique id 
     Name = "CoreLauncher",
     Description = "CoreLauncher",
-    Version = "1.0.0-alpha.9",
+    Version = "1.0.0-alpha.11",
 
     Author = {
         Developers = {
