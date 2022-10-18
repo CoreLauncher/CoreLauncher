945ac37131686e41175fb719df22f59eb67a4d78
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-10-18 10:56:37 +0200

    globals

--- src/Main/lua/ga/CoreLauncher/Main.lua
+++ src/Main/lua/ga/CoreLauncher/Main.lua
@@ -13,3 +13,14 @@ TypeWriter.Runtime.LoadJson(Resources["/Libraries/Electron-Lua.twr"])
 TypeWriter.Runtime.LoadJson(Resources["/Libraries/OpenIPC-TypeWriter.twr"])
 TypeWriter.Runtime.LoadJson(Resources["/Libraries/Static.twr"])
 end
+
+_G.CoreLauncher = {}
+CoreLauncher.IPC = Import("openipc.connector"):new("CoreLauncher", "Main")
+CoreLauncher.Electron = Import("Electron")
+CoreLauncher.RPC = Import("ga.CoreLauncher.RPC"):new("1008708322922352753")
+CoreLauncher.ApplicationData = TypeWriter.ApplicationData .. "/CoreLauncher/"
+CoreLauncher.Dev = process.env.CORELAUNCHER_DEV == "true"
+CoreLauncher.Http = Import("ga.CoreLauncher.Libraries.Http")
+CoreLauncher.Config = Import("ga.CoreLauncher.Libraries.Config"):new(CoreLauncher.ApplicationData .. "/Data2.json")
+CoreLauncher.Games = Import("ga.CoreLauncher.Games")
+CoreLauncher.API = Import("ga.CoreLauncher.API")
