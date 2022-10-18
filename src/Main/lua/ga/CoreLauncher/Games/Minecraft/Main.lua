c0f078ce509ec5ca39822d48c252accc650cf62a
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-10-18 10:52:51 +0200

    basic minecraft info

---
+++ src/Main/lua/ga/CoreLauncher/Games/Minecraft/Main.lua
@@ -0,0 +1,16 @@
+local Data = {}
+Data.Id = "MinecraftJava"
+Data.Cache = {}
+Data.Info = {
+    Name = "Minecraft",
+    LongName = "Minecraft: Java Edition",
+    Description = "Minecraft is a sandbox video game created by Swedish game developer Markus Persson and later developed by Mojang.",
+    Icon = "http://localhost:9874/assets/image/games/minecraft/icon.png",
+    Banner = "http://localhost:9874/assets/image/games/minecraft/banner.png",
+    Developer = {
+        Name = "Mojang",
+        Website = "https://www.minecraft.net/en-us/"
+    }
+}
+
+return Data
