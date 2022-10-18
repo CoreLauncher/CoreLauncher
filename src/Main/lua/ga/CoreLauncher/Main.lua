a48a4c886ca85fdf18e2297505b85af1ac5540a6
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-10-18 10:56:31 +0200

    load libs

--- src/Main/lua/ga/CoreLauncher/Main.lua
+++ src/Main/lua/ga/CoreLauncher/Main.lua
@@ -4,3 +4,12 @@ print("Hello World!")
 local FS = require("fs")
 local ApplicationData = TypeWriter.ApplicationData .. "/CoreLauncher/"
 FS.mkdirSync(ApplicationData)
+
+do
+--Load Libraries
+local Resources = TypeWriter.LoadedPackages["CoreLauncher"].Resources
+TypeWriter.Runtime.LoadJson(Resources["/Libraries/Discord-RPC.twr"])
+TypeWriter.Runtime.LoadJson(Resources["/Libraries/Electron-Lua.twr"])
+TypeWriter.Runtime.LoadJson(Resources["/Libraries/OpenIPC-TypeWriter.twr"])
+TypeWriter.Runtime.LoadJson(Resources["/Libraries/Static.twr"])
+end
