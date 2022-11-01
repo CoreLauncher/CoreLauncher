98686e3049af6749de3bda4d97a874188bba5f62
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-11-01 22:26:50 +0100

    reformat

--- Frontend/assets/script/index.js
+++ Frontend/assets/script/index.js
@@ -1,16 +1,46 @@
+const IsInIframe = window.self !== window.top
 const urlSearchParams = new URLSearchParams(window.location.search); window.QueryParameters = Object.fromEntries(urlSearchParams.entries());
-window.CoreLauncher = {
-    IPC: new OpenIPC("CoreLauncher", (document.currentScript || {getAttribute: function() {return undefined}}).getAttribute("IPC") || "Render"),
-}
 window.p = console.log
-CoreLauncher.ExtLink = async function(Link) {
-    await CoreLauncher.IPC.Send(
-        "Main",
-        "Other.ExtLink",
-        Link
+
+//#region Mouse pos
+if (IsInIframe) {
+    window.MousePosition = window.top.MousePosition
+    window.addEventListener(
+        "mousemove",
+        async function(E) {
+            const Bound = window.frameElement.getBoundingClientRect()
+            MousePosition.X = E.pageX + Bound.left
+            MousePosition.Y = E.pageY + Bound.top
+        }
+    )
+} else {
+    window.MousePosition = {}
+    window.addEventListener(
+        "mousemove",
+        async function(E) {
+            MousePosition.X = E.pageX
+            MousePosition.Y = E.pageY
+        }
     )
 }
-
+//#endregion
+//#region Main api
+if (IsInIframe) {
+    window.CoreLauncher = window.top.CoreLauncher
+} else {
+    window.CoreLauncher = {
+        IPC: new OpenIPC("CoreLauncher", (document.currentScript || {getAttribute: function() {return undefined}}).getAttribute("IPC") || "Render"),
+    }
+    CoreLauncher.ExtLink = async function(Link) {
+        await CoreLauncher.IPC.Send(
+            "Main",
+            "Other.ExtLink",
+            Link
+        )
+    }
+}
+//#endregion
+//#region additional prototypes
 String.prototype.interpolate = function(params) {
     const names = Object.keys(params);
     const vals = Object.values(params);
@@ -24,14 +54,52 @@ Number.prototype.clamp = function(min, max) {
 function Sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
-
+//#endregion
 //#region context menu
+if (IsInIframe) {
+    window.ShowContextMenu = window.top.ShowContextMenu
+    window.HideContextMenu = window.top.HideContextMenu
+    
+    
+} else {
+    const Menu = document.createElement("div")
+    Menu.classList.add("contextmenu")
+    document.body.appendChild(Menu)
+
+    window.ShowContextMenu = async function(Buttons, X = MousePosition.X, Y = MousePosition.Y) {
+        Menu.innerHTML = ''
+        for (const ContextButton of Buttons) {
+            const ContextButtonElement = document.createElement("div")
+            ContextButtonElement.classList.add("contextbutton")
+
+            const ContextLabel = document.createElement("a")
+            ContextLabel.innerText = ContextButton.Label
+            ContextLabel.classList.add("contextlabel")
+            ContextButtonElement.appendChild(ContextLabel)
+
+            const ContextImage = document.createElement("img")
+            ContextImage.src = ContextButton.Image
+            ContextImage.classList.add("contextimage")
+            ContextButtonElement.appendChild(ContextImage)
+
+            Menu.appendChild(ContextButtonElement)
+        }
+        Menu.style.top = `${Y}px`
+        Menu.style.left = `${X}px`
+    }
+    window.HideContextMenu = async function() {
+        ShowContextMenu(
+            [],
+            -500,
+            -500
+        )
+    }
+    HideContextMenu()
+}
 window.addEventListener(
-    "load",
+    "mouseup",
     async function() {
-        const Menu = document.createElement("div")
-        Menu.classList.add("contextmenu")
-        document.body.appendChild(Menu)
+        await HideContextMenu()
     }
 )
 //#endregion
