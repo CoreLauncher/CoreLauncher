2cd38f04bfc9f35e2a1a70abfea8ca215d178cf0
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-11-09 08:49:16 +0100

    more discord funcs

--- src/Main/lua/ga/CoreLauncher/API/Accounts/Discord/Main.lua
+++ src/Main/lua/ga/CoreLauncher/API/Accounts/Discord/Main.lua
@@ -4,9 +4,13 @@ local Cache
 function Discord.GetUser()
     local UserData
     if Cache then
+        p(Cache)
         UserData = Cache
     else
         local AccountData = CoreLauncher.Accounts:GetAccount("Discord")
+        if AccountData == nil then
+            return
+        end
         local Response, Body = CoreLauncher.Http.JsonRequest(
             "GET",
             "https://discordapp.com/api/users/@me",
@@ -27,7 +31,12 @@ function Discord.GetUser()
 end
 
 function Discord.GetUserIcon()
-    
+    local UserData = Discord.GetUser()
+    return string.format(
+        "https://cdn.discordapp.com/avatars/%s/%s.png",
+        UserData.id,
+        UserData.avatar
+    )
 end
 
 return Discord
