7bc2d5947e608349dc36ea1689ffa398906dd698
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-11-16 16:31:40 +0100

    reformatting and instance properties

--- Frontend/notifications/editinstance/pages/Basic/index.js
+++ Frontend/notifications/editinstance/pages/Basic/index.js
@@ -1,31 +1,51 @@
+var GameId
+var InstanceId
+
 window.addEventListener(
     "load",
     async function() {
-        const Instance = await CoreLauncher.IPC.Send(
-            "Main",
-            "Games.Instances.GetInstance",
-            {
-                GameId: QueryParameters.gameid,
-                InstanceId: QueryParameters.instanceid
-            }
-        )
+        GameId = QueryParameters.gameid
+        InstanceId = QueryParameters.instanceid
+    }
+)
+
+async function GetInstance() {
+    return await CoreLauncher.IPC.Send(
+        "Main",
+        "Games.Instances.GetInstance",
+        {
+            GameId: GameId,
+            InstanceId: InstanceId
+        }
+    )
+}
+
+async function SetProperty(PropertyType, PropertyKey, PropertyValue) {
+    await CoreLauncher.IPC.Send(
+        "Main",
+        "Games.Instances.SetProperty",
+        {
+            GameId: GameId,
+            InstanceId: InstanceId,
+            PropertyType: PropertyType,
+            PropertyKey: PropertyKey,
+            PropertyValue: PropertyValue
+        }
+    )
+}
+
+//#region Name and versions
+window.addEventListener(
+    "load",
+    async function() {
+        const Instance = await GetInstance()
         p(Instance)
 
         document.getElementById("instancename").value = Instance.Name
         document.getElementById("instancename").addEventListener(
             "change",
             async function() {
-                await CoreLauncher.IPC.Send(
-                    "Main",
-                    "Games.Instances.SetProperty",
-                    {
-                        GameId: QueryParameters.gameid,
-                        InstanceId: QueryParameters.instanceid,
-                        PropertyType: "Name",
-                        PropertyKey: "",
-                        PropertyValue: document.getElementById("instancename").value
-                    }
-                )
+                await SetProperty("Name", "", document.getElementById("instancename").value)
             }
         )
 
@@ -36,7 +56,7 @@ window.addEventListener(
             "Main",
             "Games.Instances.GetVersions",
             {
-                GameId: QueryParameters.gameid,
+                GameId: GameId,
                 Type: "LoaderVersions",
                 Value: {
                     Loader: Instance.DefaultProperties.LoaderType,
@@ -59,18 +79,60 @@ window.addEventListener(
         LoaderVersionSelect.addEventListener(
             "change",
             async function() {
-                await CoreLauncher.IPC.Send(
-                    "Main",
-                    "Games.Instances.SetProperty",
-                    {
-                        GameId: QueryParameters.gameid,
-                        InstanceId: QueryParameters.instanceid,
-                        PropertyType: "DefaultProperties",
-                        PropertyKey: "LoaderVersion",
-                        PropertyValue: LoaderVersionSelect.value
-                    }
-                )
+                await SetProperty("DefaultProperties", "LoaderVersion", LoaderVersionSelect.value)
             }
         )
     }
 )
+//#endregion
+//#region Properties
+window.addEventListener(
+    "load",
+    async function() {
+        const Instance = await GetInstance()
+        const Properties = await CoreLauncher.IPC.Send(
+            "Main",
+            "Games.Instances.GetAvailableProperties",
+            {
+                GameId: GameId,
+            }
+        )
+
+        p(Properties)
+
+        const PropertyHolder = document.getElementById("properties")
+        for (const PropertyKey in Properties) {
+            const Property = Properties[PropertyKey]
+            p(Property)
+
+            const ElementId = `InstanceProperty_${PropertyKey}`
+            var Input
+            if (Property.Type == "number") {
+                const Label = document.createElement("label")
+                Label.for = ElementId
+                Label.innerText = `${Property.Label}: `
+                PropertyHolder.appendChild(Label)
+                Input = document.createElement("input")
+                Input.type = "number"
+                Input.id = ElementId
+                Input.max = Property.Clamp.Max
+                Input.min = Property.Clamp.Min
+                Input.step = .5
+                Input.value = Instance.Properties[PropertyKey]
+                PropertyHolder.appendChild(Input)
+            }
+
+            Input.addEventListener(
+                "change",
+                async function() {
+                    p(Input.value)
+                    if (Input.value == "" || !Input.value) {
+                        Input.value = Property.Default
+                    }
+                    await SetProperty("Properties", PropertyKey, Input.value)
+                }
+            )
+        }
+    }
+)
+//#endregion
