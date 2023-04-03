aed9545a948ba838a459621abaf2c93f358312e2
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-04-03 13:58:27 +0200

    Update ApplicationLoader.lua

--- src/Main/lua/ga/corelauncher/ApplicationLoader.lua
+++ src/Main/lua/ga/corelauncher/ApplicationLoader.lua
@@ -1,13 +1,22 @@
 Import("ga.corelauncher.LoadLibraries")()
 
 local function Await(P)
-    
+    local Co = coroutine.running()
+    P:then(
+        function (...)
+            coroutine.resume(Co, ...)
+        end
+    )
+    return coroutine.yield()
 end
+
+
 
 _G.CoreLauncher = {}
 CoreLauncher.Electron = Import("electronhelper")
 CoreLauncher.ElectronApplication = CoreLauncher.Electron.app
 
-CoreLauncher.ElectronApplication:whenReady()
+print(CoreLauncher.ElectronApplication:whenReady())
+Await(CoreLauncher.ElectronApplication:whenReady())
 
 Import("ga.corelauncher")
