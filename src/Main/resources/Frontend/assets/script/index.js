8be8ceaef19f06f9756438885d4167d50817abac
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-04-08 23:24:02 +0200

    index main script stuff

--- src/Main/resources/Frontend/assets/script/index.js
+++ src/Main/resources/Frontend/assets/script/index.js
@@ -2,7 +2,14 @@ define(
     Export
 )
 
-function Export() {
+async function Export(require) {
     globalThis.CoreLauncher = {}
     CoreLauncher.ScreenManager = require("ScreenManager")
+    console.log("dsa")
+    CoreLauncher.ScreenManager.ScanScreens()
+    const sleep = ms => new Promise(r => setTimeout(r, ms));
+    await sleep(1000)
+
+    console.log("asd")
+    CoreLauncher.ScreenManager.GetScreen("main").Show()
 }
