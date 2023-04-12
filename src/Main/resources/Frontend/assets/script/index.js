a50e83a18276e39f48812d8d413201b091b4e686
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-04-12 09:50:44 +0200

    Update index.js

--- src/Main/resources/Frontend/assets/script/index.js
+++ src/Main/resources/Frontend/assets/script/index.js
@@ -4,8 +4,12 @@ await require("./lib/globals/clamp.js")
 globalThis.CoreLauncher = {}
 CoreLauncher.ScreenManager = await require("./ScreenManager.js")
 await CoreLauncher.ScreenManager.ScanScreens()
+
 const deSVG = await require("./lib/desvg.js")
 deSVG("img", true)
+await sleep(1000)
 CoreLauncher.ScreenManager.GetScreen("main").Show()
+await sleep(0.3 * 1000)
+document.getElementById("topbar").style.visibility = "visible"
 
 export default "index"
