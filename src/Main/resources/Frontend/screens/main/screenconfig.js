78eb8ce7926386c296c3fd9e462c6e58c7d94297
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-04-12 09:45:54 +0200

    expandable sidebar

--- src/Main/resources/Frontend/screens/main/screenconfig.js
+++ src/Main/resources/Frontend/screens/main/screenconfig.js
@@ -1,3 +1,30 @@
 const Screen = {}
 
+Screen.Init = function(ScreenElement) {
+    const HoverLine = ScreenElement.querySelector('.dragline')
+    const Names = ScreenElement.querySelector('.names')
+    var Dragging = false
+    const MinWidth = 200
+
+    HoverLine.addEventListener('mousedown', function() {
+        Dragging = true
+    })
+
+    document.addEventListener('mouseup', function() {
+        Dragging = false
+    })
+
+    document.addEventListener('mousemove', function(e) {
+        if (Dragging) {
+            var Width = e.clientX - 30
+            if (Width < MinWidth) {
+                Width = 0
+            } else {
+                Width = Math.clamp(Width, MinWidth, 500)
+            }
+            Names.style.width = `${Width}px`
+        }
+    })
+}
+
 export default Screen
