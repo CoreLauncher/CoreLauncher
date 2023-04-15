735d56361d826a01d1d1820b8341ebec38b8fa26
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-04-15 19:07:49 +0200

    load plugins

--- src/Main/lua/ga/corelauncher/Helpers/PluginManager/Main.lua
+++ src/Main/lua/ga/corelauncher/Helpers/PluginManager/Main.lua
@@ -5,8 +5,18 @@ local FS = TypeWriter:JsRequire("fs-extra")
 function PluginManager:LoadPlugins(PluginsFolder)
     TypeWriter.Logger:Information("Loading plugins from " .. PluginsFolder)
     local Files = FS:readdirSync(PluginsFolder)
-    print(Files)
     
+    for FileName in js.of(Files) do
+        TypeWriter.Logger:Information("Loading plugin " .. FileName)
+        local FilePath = PluginsFolder .. "/" .. FileName
+        local Plugin = TypeWriter:LoadFile(FilePath)
+        if Plugin.Entrypoints.CoreLauncherPlugin then
+            TypeWriter.Logger:Information("Plugin " .. FileName .. " is a CoreLauncher plugin")
+            local PluginData = TypeWriter:LoadEntrypoint(Plugin.Id, "CoreLauncherPlugin")
+            self.Plugins[Plugin.Id] = PluginData
+        end
+    end
+end
 end
 
 return PluginManager
