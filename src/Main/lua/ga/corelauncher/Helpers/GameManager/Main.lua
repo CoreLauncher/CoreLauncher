062267b7d43dbabae1e615630e0c540d5d5f376f
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-04-16 19:39:42 +0200

    new gamemanager

--- src/Main/lua/ga/corelauncher/Helpers/GameManager/Main.lua
+++ src/Main/lua/ga/corelauncher/Helpers/GameManager/Main.lua
@@ -1,5 +1,29 @@
 local GameManager = {}
+GameManager.Games = {}
 
+local Base64Img = TypeWriter:JsRequire("base64-img")
+local FS = TypeWriter:JsRequire("fs-extra")
 
+function GameManager:LoadGames(Games)
+    self.Games = Games
+end
+
+function GameManager:ListGames()
+    return self.Games
+end
+
+function GameManager:GetGame(GameId)
+    return self:ListGames()[GameId]
+end
+
+function GameManager:GetGameIcon(GameId)
+    local IconPath = TypeWriter.ResourceManager:GetFilePath(self:GetGame(GameId).Icon)
+    return FS:readFileSync(IconPath, "utf8")
+end
+
+function GameManager:GetGameIconBase64(GameId)
+    local IconPath = TypeWriter.ResourceManager:GetFilePath(self:GetGame(GameId).Icon)
+    return Base64Img:base64Sync(IconPath)
+end
 
 return GameManager
