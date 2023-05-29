d6110ad81eacaa0c6326bf83ae3808a6690e855f
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-05-30 00:41:48 +0200

    load banner

--- src/Main/resources/Frontend/screens/main/subscreens/genericinstanced/screenconfig.js
+++ src/Main/resources/Frontend/screens/main/subscreens/genericinstanced/screenconfig.js
@@ -2,6 +2,7 @@ const Screen = {}
 
 Screen.Init = async function(ScreenElement) {
 
+    ScreenElement.querySelector(".banner").style.backgroundImage = `url("${await CoreLauncher.GameManager.GetGameBannerBase64(Data.Id)}")`
 }
 
 export default Screen
