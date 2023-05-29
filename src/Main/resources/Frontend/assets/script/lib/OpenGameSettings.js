a94c4ed49c7f5363350a962907ab202e06a24284
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-05-30 00:31:52 +0200

    add funcs to open settings for things

---
+++ src/Main/resources/Frontend/assets/script/lib/OpenGameSettings.js
@@ -0,0 +1,24 @@
+async function OpenGameSettings(Game) {
+    const SettingsData = {}
+    SettingsData.Tabs = []
+
+    const GameSettingsGroup = {
+        Name: "Game Settings",
+        Tabs: []
+    }
+    SettingsData.Tabs.push(GameSettingsGroup)
+
+    if (Game.IsUsingInstances) {
+        GameSettingsGroup.Tabs.push(
+            {
+                Name: "Instances",
+                Screen: "instances",
+                Data: [Game.Id]
+            }
+        )
+    }
+
+    CoreLauncher.ScreenManager.GetScreen("settingswindow").Show(false, SettingsData)
+}
+
+export default OpenGameSettings
