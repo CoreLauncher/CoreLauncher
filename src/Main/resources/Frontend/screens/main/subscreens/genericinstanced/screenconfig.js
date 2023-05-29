27c53f2c766697e73fd2a4715bdc1de70d9a47c9
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-05-30 00:42:05 +0200

    settings button listener

--- src/Main/resources/Frontend/screens/main/subscreens/genericinstanced/screenconfig.js
+++ src/Main/resources/Frontend/screens/main/subscreens/genericinstanced/screenconfig.js
@@ -1,6 +1,13 @@
 const Screen = {}
 
 Screen.Init = async function(ScreenElement) {
+    ScreenElement.querySelector(".settingsbutton").addEventListener(
+        "click",
+        async function() {
+            await CoreLauncher.OpenGameSettings(Game)
+        }
+    )
+}
 
     ScreenElement.querySelector(".banner").style.backgroundImage = `url("${await CoreLauncher.GameManager.GetGameBannerBase64(Data.Id)}")`
 }
