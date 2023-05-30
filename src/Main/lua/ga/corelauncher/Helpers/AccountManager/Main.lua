9ae3c9b1d0719fb4929d7293a5c6354ec03e5c6d
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-05-30 12:17:17 +0200

    add button to connect new account

--- src/Main/lua/ga/corelauncher/Helpers/AccountManager/Main.lua
+++ src/Main/lua/ga/corelauncher/Helpers/AccountManager/Main.lua
@@ -1,4 +1,29 @@
+local AccountManager = {}
 AccountManager.AccountTypes = {}
+AccountManager.Accounts = {}
+
+local Base64Img = TypeWriter:JsRequire("base64-img")
+
 function AccountManager:LoadAccountTypes(AccountTypes)
     self.AccountTypes = AccountTypes
 end
+
+function AccountManager:LoadAccounts(AccountList)
+    
+end
+
+function AccountManager:ListAccountTypes()
+    return self.AccountTypes
+end
+
+function AccountManager:GetAccountTypeIcon(AccountTypeId)
+    local IconPath = TypeWriter.ResourceManager:GetFilePath(self.AccountTypes[AccountTypeId].Icon)
+    return FS:readFileSync(IconPath, "utf8")
+end
+
+function AccountManager:GetAccountTypeIconBase64(AccountTypeId)
+    local IconPath = TypeWriter.ResourceManager:GetFilePath(self.AccountTypes[AccountTypeId].Icon)
+    return Base64Img:base64Sync(IconPath)
+end
+
+return AccountManager
--- src/Main/resources/Frontend/screens/settingswindow/subscreens/accounts/screenconfig.js
+++ src/Main/resources/Frontend/screens/settingswindow/subscreens/accounts/screenconfig.js
@@ -1,5 +1,40 @@
 const Screen = {}
 
+Screen.Init = async function (ScreenElement, Screen) {
+    const AccountTypes = await CoreLauncher.AccountManager.ListAccountTypes()
+    console.log(AccountTypes)
+    const AccountTypeListBlock = ScreenElement.querySelector(".newaccountlist")
+    const AccountTypeList = AccountTypeListBlock.querySelector(".accounttypes")
 
+    if (Object.keys(AccountTypes).length == 0) {
+        const WarningBlock = ScreenElement.querySelector(".noaccountsblock")
+        WarningBlock.style.display = null
+        WarningBlock.style.visibility = null
+
+        AccountTypeListBlock.style.display = "none"
+        AccountTypeListBlock.style.visibility = "hidden"
+        return
+    }
+
+    const AccountTypeElementTemplate = AccountTypeListBlock.querySelector(".accounttype")
+    AccountTypeElementTemplate.remove()
+    for (const AccountTypeId in AccountTypes) {
+        const AccountType = AccountTypes[AccountTypeId]
+        const AccountTypeElement = AccountTypeElementTemplate.cloneNode(true)
+        AccountTypeElement.querySelector("img").src = await CoreLauncher.AccountManager.GetAccountTypeIconBase64(AccountTypeId)
+        AccountTypeElement.querySelector("a").innerText = AccountType.Name
+        AccountTypeList.appendChild(AccountTypeElement)
+    }
+
+}
+
+Screen.Show = async function (ScreenElement, Screen) {
+    ScreenElement.querySelectorAll(".accounttype.fullwidth").forEach(
+        function (Element) {
+            Element.style.setProperty("--hover-width", Element.clientWidth + 1 + "px")
+            Element.classList.remove("fullwidth")
+        }
+    )
+}
 
 export default Screen
