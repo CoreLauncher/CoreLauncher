70f75b260ebd9ce64eaf499cf39cbf14cef2baba
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-06-12 15:40:44 +0200

    List account on generic page

--- src/Main/resources/Frontend/screens/main/subscreens/genericinstanced/screenconfig.js
+++ src/Main/resources/Frontend/screens/main/subscreens/genericinstanced/screenconfig.js
@@ -1,19 +1,53 @@
 const Screen = {}
 
 var Game
-Screen.Init = async function(ScreenElement) {
+var AccountSelector
+var InstanceSelector
+Screen.Init = async function (ScreenElement) {
     ScreenElement.querySelector(".settingsbutton").addEventListener(
         "click",
-        async function() {
+        async function () {
             await CoreLauncher.OpenGameSettings(Game)
         }
     )
+
+    AccountSelector = ScreenElement.querySelector(".accountselector")
+    InstanceSelector = ScreenElement.querySelector(".instanceselector")
 }
 
-Screen.Show = async function(ScreenElement, Screen, Data) {
+Screen.Show = async function (ScreenElement, Screen, Data) {
     Game = Data
 
     ScreenElement.querySelector(".banner").style.backgroundImage = `url("${await CoreLauncher.GameManager.GetGameBannerBase64(Data.Id)}")`
+
+    if (Game.UsesInstances) {
+        InstanceSelector.style.display = "block"
+        InstanceSelector.style.visibility = "visible"
+    } else {
+        InstanceSelector.style.display = "none"
+        InstanceSelector.style.visibility = "hidden"
+    }
+
+    if (Game.UsesAccounts) {
+        AccountSelector.style.display = "block"
+        AccountSelector.style.visibility = "visible"
+
+        AccountSelector.innerHTML = ""
+        var Accounts = await CoreLauncher.GameManager.GetValidAccounts(Game.Id)
+        console.log(Accounts)
+        if (!Array.isArray(Accounts)) { Accounts = [] }
+
+        for (const Account of Accounts) {
+            console.log(Account)
+            const OptionElement = document.createElement("option")
+            OptionElement.value = Account.Id
+            OptionElement.innerText = Account.DisplayData.Name
+            AccountSelector.appendChild(OptionElement)
+        }
+    } else {
+        AccountSelector.style.display = "none"
+        AccountSelector.style.visibility = "hidden"
+    }
 }
 
 export default Screen
