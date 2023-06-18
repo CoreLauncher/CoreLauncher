d5d27242e16eec06465c80c3d939ebc8f4ef15a4
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-06-19 00:33:57 +0200

    make cancel button redirect back

--- src/Main/resources/Frontend/screens/settingswindow/subscreens/instances/subscreens/createinstance/screenconfig.js
+++ src/Main/resources/Frontend/screens/settingswindow/subscreens/instances/subscreens/createinstance/screenconfig.js
@@ -1,5 +1,17 @@
 const Screen = {}
 
+var Game
+Screen.Init = function (ScreenElement, Screen) {
+    ScreenElement.querySelector(".cancelbutton").addEventListener(
+        "click",
+        function () {
+            Screen.ParentScreen.GetScreen("instancelist").Show(false, Game)
+        }
+    )
+}
 
+Screen.Show = function (ScreenElement, Screen, Data) {
+    Game = Data
+}
 
 export default Screen
