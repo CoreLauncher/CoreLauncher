aebfd9a37306e2008ffb13e9457f73b112559ddc
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-06-19 00:33:23 +0200

    make createinstance button redirect to create page

--- src/Main/resources/Frontend/screens/settingswindow/subscreens/instances/subscreens/instancelist/screenconfig.js
+++ src/Main/resources/Frontend/screens/settingswindow/subscreens/instances/subscreens/instancelist/screenconfig.js
@@ -1,7 +1,17 @@
 const Screen = {}
 
+var Game
 Screen.Init = function (ScreenElement, Screen) {
+    ScreenElement.querySelector(".createinstancebutton").addEventListener(
+        "click",
+        function () {
+            Screen.ParentScreen.GetScreen("createinstance").Show(false, Game)
+        }
+    )
+}
 
+Screen.Show = function (ScreenElement, Screen, Data) {
+    Game = Data
 }
 
 export default Screen
