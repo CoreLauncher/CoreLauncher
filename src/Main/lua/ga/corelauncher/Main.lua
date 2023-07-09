5e2232caa8f20d02a27a68fdc9f7fbe372805776
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-07-09 22:50:49 +0200

    various fixes

--- src/Main/lua/ga/corelauncher/Main.lua
+++ src/Main/lua/ga/corelauncher/Main.lua
@@ -1,9 +1,9 @@
 _G.Object = function(t)
-    if type(t) ~= "table" then
+    if type(t) ~= "table" or true then
         return t
     end
 
-    local o = js.new(js.global.Object)
+    local o = TypeWriter.JavaScript:New(TypeWriter.JavaScript.Global.Object)
     for k, v in pairs(t) do
         assert(type(k) == "string" or js.typeof(k) == "symbol", "JavaScript only has string and symbol keys")
         if type(v) == "table" then
@@ -15,11 +15,11 @@ _G.Object = function(t)
 end
 
 _G.Array = function(t)
-    if type(t) ~= "table" then
+    if type(t) ~= "table" or true then
         return t
     end
 
-    local a = js.new(js.global.Array)
+    local a = TypeWriter.JavaScript:New(TypeWriter.JavaScript.Global.Array)
     for i, v in ipairs(t) do
         if type(v) == "table" then
             v = ToJs(v)
@@ -57,13 +57,13 @@ _G.ToLua = function(o)
         end
     end
 
-    if js.typeof(o) == "object" then
+    if TypeWriter.JavaScript.Global:Object(o) == "object" then
         local t = {}
         for k, v in pairs(o) do
             t[k] = ToLua(v)
         end
         return t
-    elseif js.typeof(o) == "array" then
+    elseif TypeWriter.JavaScript.Global:Object(o) == "array" then
         local t = {}
         for i = 0, o.length - 1 do
             t[i + 1] = ToLua(o[i])
@@ -74,55 +74,51 @@ _G.ToLua = function(o)
     end
 end
 
-local FS = TypeWriter:JsRequire("fs-extra")
-local Base64 = TypeWriter:JsRequire("js-base64")
-local Json = Js.global.JSON
+local FS = TypeWriter.JavaScript:Require("fs-extra")
+local Base64 = TypeWriter.JavaScript:Require("js-base64")
+local Json = TypeWriter.JavaScript.Global.JSON
 
 _G.Inspect = function(O)
-    print(Import("ga.corelauncher.Libraries.Inspect")(ToLua(O)))
+    return TypeWriter.JavaScript.Global.console:log(O)
 end
 _G.CoreLauncher = {}
-js.global.CoreLauncher = _G.CoreLauncher
-CoreLauncher.DevMode = js.global.process.env.CORELAUNCHER_DEV == "true"
+TypeWriter.JavaScript.Global.CoreLauncher = _G.CoreLauncher
+CoreLauncher.DevMode = TypeWriter.JavaScript.Global.process.env.CORELAUNCHER_DEV == "true"
 CoreLauncher.ApplicationData = TypeWriter.ApplicationData .. "/CoreLauncher/"
 CoreLauncher.PluginsFolder = CoreLauncher.ApplicationData .. "/Plugins/"
 FS:ensureDirSync(CoreLauncher.ApplicationData)
 FS:ensureDirSync(CoreLauncher.PluginsFolder)
 
-CoreLauncher.AccountManager = Import("ga.corelauncher.Classes.AccountManager")
-CoreLauncher.DataBase = js.new(Import("ga.corelauncher.DataBase"), CoreLauncher.ApplicationData .. "/Database.json")
-CoreLauncher.PluginManager = Import("ga.corelauncher.Classes.PluginManager")
-CoreLauncher.GameManager = Import("ga.corelauncher.Classes.GameManager")
-CoreLauncher.WindowControl = Import("ga.corelauncher.Classes.WindowControl")
+CoreLauncher.AccountManager = TypeWriter.JavaScript.New(Import("ga.corelauncher.Classes.AccountManager"))
+CoreLauncher.DataBase = TypeWriter.JavaScript.New(Import("ga.corelauncher.DataBase"), CoreLauncher.ApplicationData .. "/Database.json")
+CoreLauncher.PluginManager = TypeWriter.JavaScript.New(Import("ga.corelauncher.Classes.PluginManager"))
+CoreLauncher.GameManager = TypeWriter.JavaScript.New(Import("ga.corelauncher.Classes.GameManager"))
+CoreLauncher.WindowControl = TypeWriter.JavaScript.New(Import("ga.corelauncher.Classes.WindowControl"))
 
 CoreLauncher.Electron = Import("electronhelper")()
 CoreLauncher.ElectronApplication = CoreLauncher.Electron.app
-Await(CoreLauncher.ElectronApplication:whenReady())
+CoreLauncher.ElectronApplication.whenReady():await()
 
 CoreLauncher.IPCMain = CoreLauncher.Electron.ipcMain
-CoreLauncher.BrowserWindow = jsnew(
+CoreLauncher.BrowserWindow = TypeWriter.JavaScript.New(
     CoreLauncher.Electron.BrowserWindow,
-    Object(
-        {
-            show = false,
-            frame = false,
-            center = true,
-            titleBarStyle = "hidden",
+    {
+        show = false,
+        frame = false,
+        center = true,
+        titleBarStyle = "hidden",
 
-            width = 275,
-            height = 400,
-            minWidth = 1000,
-            minHeight = 600,
+        width = 275,
+        height = 400,
+        minWidth = 1000,
+        minHeight = 600,
 
-            webPreferences = Object(
-                {
-                    preload = TypeWriter.ResourceManager:GetFilePath("CoreLauncher", "/Frontend/preload.js")
-                }
-            )
+        webPreferences = {
+            preload = TypeWriter.ResourceManager:GetFilePath("CoreLauncher", "/Frontend/preload.js")
         }
-    )
+    }
 )
-CoreLauncher.BrowserWindow:setResizable(false)
+CoreLauncher.BrowserWindow.setResizable(false)
 
 Import("ga.corelauncher.ipc.pipes.accountmanager")
 Import("ga.corelauncher.ipc.pipes.database")
@@ -131,7 +127,6 @@ Import("ga.corelauncher.ipc.pipes.pluginmanager")
 Import("ga.corelauncher.ipc.pipes.windowcontrol")
 
 CoreLauncher.StaticServer = Import("me.corebyte.static")(
-    "",
     9875,
     "CoreLauncher",
     "Frontend",
@@ -149,20 +144,20 @@ CoreLauncher.StaticServer = Import("me.corebyte.static")(
 
 if CoreLauncher.DevMode then
     TypeWriter.Logger:Warning("We are running in dev env")
-    CoreLauncher.BrowserWindow:loadURL("http://localhost:9874")
-    CoreLauncher.BrowserWindow:openDevTools()
+    CoreLauncher.BrowserWindow.loadURL("http://localhost:9874")
+    CoreLauncher.BrowserWindow.openDevTools()
 else
-    CoreLauncher.BrowserWindow:loadURL("http://localhost:9875")
+    CoreLauncher.BrowserWindow.loadURL("http://localhost:9875")
 end
 
-CoreLauncher.BrowserWindow:once(
+CoreLauncher.PluginManager:LoadPlugins(CoreLauncher.PluginsFolder)
+CoreLauncher.GameManager:LoadGames(CoreLauncher.PluginManager:ListGames())
+CoreLauncher.AccountManager:LoadAccountTypes(CoreLauncher.PluginManager:ListAccountTypes())
+
+CoreLauncher.BrowserWindow.once(
     "ready-to-show",
     function()
         CoreLauncher.BrowserWindow:show()
-
-        CoreLauncher.PluginManager:LoadPlugins(CoreLauncher.PluginsFolder)
-        CoreLauncher.GameManager:LoadGames(CoreLauncher.PluginManager:ListGames())
-        CoreLauncher.AccountManager:LoadAccountTypes(CoreLauncher.PluginManager:ListAccountTypes())
 
         CoreLauncher.BrowserWindow:setResizable(true)
         CoreLauncher.BrowserWindow:setSize(1000, 600)
@@ -170,8 +165,11 @@ CoreLauncher.BrowserWindow:once(
     end
 )
 
-CoreLauncher.BrowserWindow:on("resize", function()
-    local Size = CoreLauncher.BrowserWindow:getSize()
-    CoreLauncher.DataBase:SetKey("Window.Width", Size[0])
-    CoreLauncher.DataBase:SetKey("Window.Height", Size[1])
-end)
+CoreLauncher.BrowserWindow.on(
+    "resize",
+    function()
+        local Size = CoreLauncher.BrowserWindow.getSize()
+        CoreLauncher.DataBase:SetKey("Window.Width", Size[0])
+        CoreLauncher.DataBase:SetKey("Window.Height", Size[1])
+    end
+)
