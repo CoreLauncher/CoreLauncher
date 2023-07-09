07e6e4e5cb08c55edff8352a60590839ba684836
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-07-09 22:50:07 +0200

    async fix

--- src/Main/js/ga/corelauncher/LoadLibraries.js
+++ src/Main/js/ga/corelauncher/LoadLibraries.js
@@ -1,17 +1,20 @@
 TypeWriter.LoadFile(TypeWriter.ResourceManager.GetFilePath("CoreLauncher", "/Libraries/ElectronHelper.twr"))
 TypeWriter.LoadFile(TypeWriter.ResourceManager.GetFilePath("CoreLauncher", "/Libraries/Static.twr"))
 
-const PluginManagerClass = Import("ga.corelauncher.Classes.PluginManager")
-const PluginManager = new PluginManagerClass()
-PluginManager.LoadPlugins(TypeWriter.ApplicationData + "/CoreLauncher/Plugins/")
+ImportAsync("ga.corelauncher.Classes.PluginManager").then(
+    (PluginManagerClass) => {
+        const PluginManager = new PluginManagerClass()
+        PluginManager.LoadPlugins(TypeWriter.ApplicationData + "/CoreLauncher/Plugins/")
 
-Import("electronhelper")(
-    "CoreLauncher",
-    {
-        Name: "CoreLauncher",
-        Icon: {
-            win32: "CoreLauncher:/ico.ico"
-        },
-        Load: "ga.corelauncher"
+        Import("electronhelper")(
+            "CoreLauncher",
+            {
+                Name: "CoreLauncher",
+                Icon: {
+                    win32: "CoreLauncher:/ico.ico"
+                },
+                Load: "ga.corelauncher"
+            }
+        )
     }
-)    
+)
