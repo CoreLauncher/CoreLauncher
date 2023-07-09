7ebd5f15065c32cfceed23c52d0ca8965d846204
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-07-09 22:50:16 +0200

    Delete FetchJson.lua

--- src/Main/lua/ga/corelauncher/Libraries/FetchJson.lua
+++
@@ -1,9 +0,0 @@
-local NodeFetch = TypeWriter:JsRequire("node-fetch")
-
-local function FetchJson(Url)
-    local _, Response = Await(NodeFetch(NodeFetch, Url))
-    local _, Json = Await(Response:json())
-    return Json
-end
-
-return FetchJson
