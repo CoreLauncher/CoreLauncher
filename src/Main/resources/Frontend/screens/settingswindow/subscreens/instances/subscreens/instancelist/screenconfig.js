f9c294ab8c0e9974c0458df3bd671699feaaad12
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-07-16 14:54:01 +0200

    complete instancelist code

--- src/Main/resources/Frontend/screens/settingswindow/subscreens/instances/subscreens/instancelist/screenconfig.js
+++ src/Main/resources/Frontend/screens/settingswindow/subscreens/instances/subscreens/instancelist/screenconfig.js
@@ -1,17 +1,41 @@
 const Screen = {}
 
-var Game
+var GameId
+var InstanceList
+var InstanceTemplate
 Screen.Init = function (ScreenElement, Screen) {
+    InstanceList = ScreenElement.querySelector(".instancelist")
+    InstanceTemplate = ScreenElement.querySelector(".instance")
+    InstanceTemplate.remove()
+
     ScreenElement.querySelector(".createinstancebutton").addEventListener(
         "click",
         function () {
-            Screen.ParentScreen.GetScreen("createinstance").Show(false, Game)
+            Screen.ParentScreen.GetScreen("createinstance").Show(false, GameId)
         }
     )
 }
 
-Screen.Show = function (ScreenElement, Screen, Data) {
-    Game = Data
+Screen.Show = async function (ScreenElement, Screen, Data) {
+    GameId = Data
+
+    InstanceList.innerHTML = ""
+
+    const Instances = await CoreLauncher.GameManager.ListInstances(GameId)
+    for (const Instance of Instances) {
+        const InstanceElement = InstanceTemplate.cloneNode(true)
+        InstanceElement.querySelector(".name").innerText = Instance.Name
+        InstanceElement.querySelector(".icon").src = await CoreLauncher.GameManager.GetGameIconBase64(GameId)
+
+        InstanceElement.addEventListener(
+            "click",
+            function () {
+                CoreLauncher.Settings.OpenInstanceSettings(GameId, Instance.UUID)
+            }
+        )
+        
+        InstanceList.appendChild(InstanceElement)
+    }
 }
 
 export default Screen
