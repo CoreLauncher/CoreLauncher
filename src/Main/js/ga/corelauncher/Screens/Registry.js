1d8b951c25e38fef7d850ddc11c1f5890d332cd1
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-11-27 23:55:09 +0100

    add loadingscreen

--- src/Main/js/ga/corelauncher/Screens/Registry.js
+++ src/Main/js/ga/corelauncher/Screens/Registry.js
@@ -1,3 +1,4 @@
 return async function(ScreenManager) {
+    ScreenManager.RegisterScreen("LoadingScreen", await Import("ga.corelauncher.Screens.LoadingScreen"))
     ScreenManager.RegisterScreen("Main", await Import("ga.corelauncher.Screens.Main"))
 }
