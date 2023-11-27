6e85baeecfeac76795595d7dfc4282332e71f425
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-11-27 23:55:12 +0100

    Create Main.js

---
+++ src/Main/js/ga/corelauncher/Screens/LoadingScreen/Main.js
@@ -0,0 +1,15 @@
+return {
+    Default: true,
+
+    Init: async function(Screen, ScreenElement, ScreenManager) {
+
+    },
+
+    Show: async function(Screen, ScreenElement, ScreenManager) {
+    
+    },
+    
+    Hide: async function(Screen, ScreenElement, ScreenManager) {
+    
+    }
+}
