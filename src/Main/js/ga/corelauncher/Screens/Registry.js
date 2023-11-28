696fff7271111c0bad5bc87ff607332ceaf72b42
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-11-28 14:56:05 +0100

    await registerscreen

--- src/Main/js/ga/corelauncher/Screens/Registry.js
+++ src/Main/js/ga/corelauncher/Screens/Registry.js
@@ -1,4 +1,4 @@
 return async function(ScreenManager) {
-    ScreenManager.RegisterScreen("LoadingScreen", await Import("ga.corelauncher.Screens.LoadingScreen"))
-    ScreenManager.RegisterScreen("Main", await Import("ga.corelauncher.Screens.Main"))
+    await ScreenManager.RegisterScreen("LoadingScreen", await Import("ga.corelauncher.Screens.LoadingScreen"))
+    await ScreenManager.RegisterScreen("Main", await Import("ga.corelauncher.Screens.Main"))
 }
