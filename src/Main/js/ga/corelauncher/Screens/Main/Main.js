9053902368b61675ae13e5ebcba83901fca994be
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-11-28 14:55:55 +0100

    add window controls

--- src/Main/js/ga/corelauncher/Screens/Main/Main.js
+++ src/Main/js/ga/corelauncher/Screens/Main/Main.js
@@ -1,14 +1,21 @@
 return {
-    
+    Default: false,
 
     Init: async function(Screen, ScreenElement, ScreenManager) {
+        const NwWindow = nw.Window.get()
+
+        ScreenElement.querySelector(".close").addEventListener("click", NwWindow.close.bind(NwWindow))
+        ScreenElement.querySelector(".minimize").addEventListener("click", NwWindow.minimize.bind(NwWindow))
+        ScreenElement.querySelector(".maximize").addEventListener("click", NwWindow.maximize.bind(NwWindow))
 
     },
 
+    ApplyShowStyle: true,
     Show: async function(Screen, ScreenElement, ScreenManager) {
     
     },
     
+    ApplyHideStyle: true,
     Hide: async function(Screen, ScreenElement, ScreenManager) {
     
     }
