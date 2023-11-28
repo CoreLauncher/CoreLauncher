813698df35728d722f080707a62e9cc509b5f73c
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-11-28 15:44:56 +0100

    register gameslist

--- src/Main/js/ga/corelauncher/Screens/Registry.js
+++ src/Main/js/ga/corelauncher/Screens/Registry.js
@@ -1,4 +1,5 @@
 return async function(ScreenManager) {
     await ScreenManager.RegisterScreen("LoadingScreen", await Import("ga.corelauncher.Screens.LoadingScreen"))
     await ScreenManager.RegisterScreen("Main", await Import("ga.corelauncher.Screens.Main"))
+    await ScreenManager.RegisterScreen("Main.GamesList", await Import("ga.corelauncher.Screens.Main.GamesList"))
 }
