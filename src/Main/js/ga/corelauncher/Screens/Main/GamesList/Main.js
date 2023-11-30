f1b450ddf21723e59bcd96f8f5235619a838d13a
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-11-30 11:00:30 +0100

    add gameslist

--- src/Main/js/ga/corelauncher/Screens/Main/GamesList/Main.js
+++ src/Main/js/ga/corelauncher/Screens/Main/GamesList/Main.js
@@ -1,17 +1,54 @@
 return {
     Default: true,
 
-    Init: async function(Screen, ScreenElement, ScreenManager) {
+    Init: async function(Screen, ScreenElement, ScreenManager, Data) {
 
     },
 
     ApplyShowStyle: true,
-    Show: async function(Screen, ScreenElement, ScreenManager) {
+    Show: async function(Screen, ScreenElement, ScreenManager, Data) {
         document.querySelector(".topbar .imageholder").classList.add("showbars")
+
+        const IconsColumn = ScreenElement.querySelector(".iconcolumn")
+
+        const Games = CoreLauncher.GameManager.ListGames()
+
+        console.log(Games)
+
+        IconsColumn.innerHTML = ""
+        for (const Game of Games) {
+            console.log(Game)
+
+            const IconHolder = document.createElement("div")
+            IconHolder.classList.add("icon")
+            IconsColumn.appendChild(IconHolder)
+
+            const Icon = document.createElement("img")
+            Icon.src = Game.GetIconBase64()
+            IconHolder.appendChild(Icon)
+
+            IconHolder.addEventListener(
+                "click",
+                async function () {
+                    if (IconHolder.classList.contains("selected")) { return }
+                    CoreLauncher.DataBase.SetKey("States.GamesList.SelectedGame", Game.Id)
+                    IconsColumn.querySelector(".selected")?.classList.remove("selected")
+                    IconHolder.classList.add("selected")
+                    await Screen.GetScreen(Game.PlayScreenType).Show(Game)
+                }
+            )
+
+            const SelectedGame = CoreLauncher.DataBase.GetKey("States.GamesList.SelectedGame")
+            if (SelectedGame == Game.Id || SelectedGame == undefined) {
+                IconHolder.click()
+            }
+
+
+        }
     },
     
     ApplyHideStyle: true,
-    Hide: async function(Screen, ScreenElement, ScreenManager) {
+    Hide: async function(Screen, ScreenElement, ScreenManager, Data) {
         document.querySelector(".topbar .imageholder").classList.remove("showbars")
     }
 }
