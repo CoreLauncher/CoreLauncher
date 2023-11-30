d9314d66c544f006a1dfbccbcdd8da2c888caec0
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-11-30 13:59:49 +0100

    add settings screen

---
+++ src/Main/js/ga/corelauncher/Screens/Main/Settings/Main.js
@@ -0,0 +1,16 @@
+return {
+    Default: false,
+
+    Init: async function(Screen, ScreenElement, ScreenManager, Data) {
+
+    },
+
+    ApplyShowStyle: true,
+    Show: async function(Screen, ScreenElement, ScreenManager, Data) {
+    },
+    
+    ApplyHideStyle: true,
+    Hide: async function(Screen, ScreenElement, ScreenManager, Data) {
+
+    }
+}
