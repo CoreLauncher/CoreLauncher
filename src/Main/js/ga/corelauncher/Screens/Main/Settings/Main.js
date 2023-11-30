e899ca643f5c8606b016f2cc9f1c619413786f17
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-12-01 00:17:01 +0100

    finish settings page

--- src/Main/js/ga/corelauncher/Screens/Main/Settings/Main.js
+++ src/Main/js/ga/corelauncher/Screens/Main/Settings/Main.js
@@ -1,13 +1,57 @@
+let ReturnData
+let TabTemplate
+
 return {
     Default: false,
 
     Init: async function(Screen, ScreenElement, ScreenManager, Data) {
+        ScreenElement.querySelector(".returnbutton").addEventListener(
+            "click",
+            function () {
+                if (!ReturnData) { return }
+                ReturnData.Screen.Show(ReturnData.Data)
+                ReturnData = undefined
+            }
+        )
 
+        TabTemplate = ScreenElement.querySelector(".tab")
+        TabTemplate.remove()
     },
 
     ApplyShowStyle: true,
     Show: async function(Screen, ScreenElement, ScreenManager, Data) {
-        console.log(Data)
+        if (!Data.ReturnData) {
+            Data.ReturnData = {
+                Screen: ScreenManager.GetScreen("Main.GamesList"),
+                Data: undefined
+            }
+        }
+        ReturnData = Data.ReturnData
+
+        const TabsList = ScreenElement.querySelector(".tabslist")
+        TabsList.innerHTML = ""
+        for (const Tab of Data.Tabs) {
+            if (typeof Tab === "string") {
+                const TabHeaderElement = document.createElement("a")
+                TabHeaderElement.classList.add("tabheader")
+                TabHeaderElement.innerText = Tab
+                TabsList.appendChild(TabHeaderElement)
+            } else {
+                const TabElement = TabTemplate.cloneNode(true)
+                TabElement.querySelector("a").innerText = Tab.Name
+                const TabScreen = Screen.GetScreen(Tab.Screen)
+                TabElement.addEventListener(
+                    "click",
+                    function () {
+                        const ActiveTab = TabsList.querySelector(".tab.active")
+                        if (ActiveTab) { ActiveTab.classList.remove("active") }
+                        TabElement.classList.add("active")
+                        TabScreen.Show(Tab.Data)
+                    }
+                )
+                TabsList.appendChild(TabElement)
+            }
+        }
     },
     
     ApplyHideStyle: true,
