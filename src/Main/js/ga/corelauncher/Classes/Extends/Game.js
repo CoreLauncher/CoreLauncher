792feb9d2ff54bd097ef8d0f63d71f2aff42c5b5
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-12-01 10:56:14 +0100

    add & update methods

--- src/Main/js/ga/corelauncher/Classes/Extends/Game.js
+++ src/Main/js/ga/corelauncher/Classes/Extends/Game.js
@@ -2,21 +2,28 @@ const ResourceBase64 = await Import("ga.corelauncher.Helpers.ResourceBase64")
 
 class Game {
     constructor() {
-        CoreLauncher.DataBase.SetKeyIfNotExists(`Game.${this.Id}.Properties`, this.DefaultGameProperties())
+        CoreLauncher.DataBase.SetKeyIfNotExists(`Game.${this.Id}.Properties`, this.DefaultProperties())
         CoreLauncher.DataBase.SetKeyIfNotExists(`Game.${this.Id}.Instances`, {})
     }
 
-    DefaultGameProperties() {
-        const Properties = this.GameProperties()
+    DefaultProperties() {
         const DefaultProperties = {}
 
-        for (const PropertyRow of Properties) {
+        for (const PropertyRow of this.Properties) {
             for (const Property of PropertyRow) {
                 DefaultProperties[Property.Id] = Property.Default
             }
         }
 
         return DefaultProperties
+    }
+
+    SetProperties(Properties) {
+        CoreLauncher.DataBase.SetKey(`Game.${this.Id}.Properties`, Properties)
+    }
+
+    GetProperties(FillDefaults=false) {
+        return CoreLauncher.DataBase.GetKey(`Game.${this.Id}.Properties`)
     }
 
     GetIconBase64() {
