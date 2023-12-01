3f59d067ed4a9dff077b29c23903a0606366b097
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-12-01 23:40:27 +0100

    save src

--- src/Main/js/ga/corelauncher/Helpers/DeSVG.js
+++ src/Main/js/ga/corelauncher/Helpers/DeSVG.js
@@ -8,7 +8,8 @@ async function ReplaceImage(ImageTag, SvgUrl) {
     const Response = await fetch(SvgUrl)
     const Svg = await Response.text()
     const SvgElement = ElementFromXml(Svg)
-    SvgElement.setAttribute("class", ImageTag.getAttribute("class"))
+    SvgElement.setAttribute("src", SvgUrl)
+    ImageTag.getAttribute("class") ? SvgElement.setAttribute("class", ImageTag.getAttribute("class")) : null
     ImageTag.replaceWith(SvgElement)
 }
 
