e2dae4d60f961c8dadc4f5187ab9303ac4a77486
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-12-02 16:35:11 +0100

    add accounts page

--- src/Main/js/ga/corelauncher/Screens/Registry.js
+++ src/Main/js/ga/corelauncher/Screens/Registry.js
@@ -5,6 +5,7 @@ return async function(ScreenManager) {
     await ScreenManager.RegisterScreen("Main.GamesList.Generic", await Import("ga.corelauncher.Screens.Main.GamesList.Generic"))
     await ScreenManager.RegisterScreen("Main.Settings", await Import("ga.corelauncher.Screens.Main.Settings"))
     await ScreenManager.RegisterScreen("Main.Settings.About", await Import("ga.corelauncher.Screens.Main.Settings.About"))
+    await ScreenManager.RegisterScreen("Main.Settings.Accounts", await Import("ga.corelauncher.Screens.Main.Settings.Accounts"))
     await ScreenManager.RegisterScreen("Main.Settings.GameInformation", await Import("ga.corelauncher.Screens.Main.Settings.GameInformation"))
     await ScreenManager.RegisterScreen("Main.Settings.Instances", await Import("ga.corelauncher.Screens.Main.Settings.Instances"))
 }
