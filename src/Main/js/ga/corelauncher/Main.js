9a95206d6afd1b69bfbba84134a9c5e676b7b177
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-12-03 21:15:24 +0100

    create main class

--- src/Main/js/ga/corelauncher/Main.js
+++ src/Main/js/ga/corelauncher/Main.js
@@ -1,32 +1,7 @@
 const FS = require("fs-extra")
+const Path = require("path")
 
-globalThis.CoreLauncher = {}
-CoreLauncher.DevMode = process.env.CORELAUNCHER_DEV == "true"
-CoreLauncher.ApplicationData = `${TypeWriter.ApplicationData}/CoreLauncher`
-CoreLauncher.PluginsFolder = `${CoreLauncher.ApplicationData}/Plugins`
-CoreLauncher.PluginDataFolder = `${CoreLauncher.ApplicationData}/PluginData`
-
-FS.ensureDirSync(CoreLauncher.ApplicationData)
-FS.ensureDirSync(CoreLauncher.PluginsFolder)
-FS.ensureDirSync(CoreLauncher.PluginDataFolder)
-
-
-
-window.onbeforeunload = function () {
-    console.log("unloading")
-    CoreLauncher.StaticServer.Close()
-}
-
-nw.Window.get().on(
-    "close",
-    function () {
-        console.log("closing")
-        CoreLauncher.StaticServer.Close()
-        nw.Window.get().close(true)
-    }
-)
-
-CoreLauncher.Logger = TypeWriter.CreateLogger("CoreLauncher")
+const WaitForEvent = await Import("ga.corelauncher.Helpers.WaitForEvent")
 
 const DataBaseClass = await Import("ga.corelauncher.Classes.DataBase")
 const ScreenManagerClass = await Import("ga.corelauncher.Classes.ScreenManager")
@@ -36,23 +11,95 @@ const GameManagerClass = await Import("ga.corelauncher.Classes.GameManager")
 const TaskManagerClass = await Import("ga.corelauncher.Classes.TaskManager")
 const WindowControlClass = await Import("ga.corelauncher.Classes.WindowControl")
 
-document.addEventListener(
-    "DOMContentLoaded",
-    async function () {
-        CoreLauncher.DataBase = new DataBaseClass(`${CoreLauncher.ApplicationData}/Database.json`)
-        CoreLauncher.ScreenManager = new ScreenManagerClass()
-        await (await Import("ga.corelauncher.Screens.Registry"))(CoreLauncher.ScreenManager)
-        await (await Import("ga.corelauncher.Helpers.DeSVG"))()
+class CoreLauncherClass {
+    constructor() {
+        this.DevMode = process.env.CORELAUNCHER_DEV == "true"
+        this.Logger = TypeWriter.CreateLogger("CoreLauncher")
 
-        CoreLauncher.PluginManager = new PluginManagerClass(CoreLauncher.PluginsFolder)
-        await CoreLauncher.PluginManager.LoadPlugins()
-        CoreLauncher.AccountManager = new AccountManagerClass(CoreLauncher.PluginManager.ListAccountTypes())
-        CoreLauncher.GameManager = new GameManagerClass(CoreLauncher.PluginManager.ListGames())
-        CoreLauncher.TaskManager = new TaskManagerClass()
-        CoreLauncher.WindowControl = new WindowControlClass()
+        //Folders
+        this.Logger.Information("Loading folders")
+        this.ApplicationData = Path.normalize(`${TypeWriter.ApplicationData}/CoreLauncher`)
+        this.PluginsFolder = Path.normalize(`${this.ApplicationData}/Plugins`)
+        this.PluginDataFolder = Path.normalize(`${this.ApplicationData}/PluginData`)
+        FS.ensureDirSync(this.ApplicationData)
+        FS.ensureDirSync(this.PluginsFolder)
+        FS.ensureDirSync(this.PluginDataFolder)
 
-        // await CoreLauncher.ScreenManager.GetScreen("Main").Show()
-        await CoreLauncher.GameManager.Games[0].OpenSettings()
-        
+        //Load Classes
+        this.Logger.Information("Loading classes")
+        this.DataBase = new DataBaseClass(`${this.ApplicationData}/Database.json`)
+
+        this.Plugins = {}
     }
-)
+
+    async AsyncLoad() {
+        //Register Screens
+        this.ScreenManager = new ScreenManagerClass()
+        const ScreenRegistry = await Import("ga.corelauncher.Screens.Registry")
+        await ScreenRegistry(this.ScreenManager)
+    }
+
+    async LoadPlugins() {
+        CoreLauncher.Logger.Information(`Loading plugins from ${this.PluginsFolder}`)
+        const Files = FS.readdirSync(this.PluginsFolder)
+        for (const FileName of Files) {
+            const FilePath = Path.normalize(`${this.PluginsFolder}/${FileName}`)
+            const Plugin = await TypeWriter.LoadFile(FilePath)
+            if (!Plugin.PackageInfo.Entrypoints.CoreLauncherPlugin) { continue }
+            CoreLauncher.Logger.Information(`Loading plugin ${Plugin.PackageInfo.Id}`)
+            const PluginDataFolder = `${this.PluginDataFolder}/${Plugin.PackageInfo.Id}`
+            FS.ensureDirSync(PluginDataFolder)
+
+            const PluginClass = await Plugin.LoadEntrypoint("CoreLauncherPlugin")
+            const PluginInstance = new PluginClass(PluginDataFolder)
+            console.log(PluginInstance)
+            this.Plugins[Plugin.PackageInfo.Id] = PluginInstance
+            await PluginInstance.Load()
+        }
+    }
+
+    ListGames() {
+        return Object.values(this.Plugins).flatMap(Plugin => Plugin.Games)
+    }
+    
+    GetGame(Id) {
+        return this.ListGames().find(Game => Game.Id == Id)
+    }
+
+    ListAccountTypes() {
+        return Object.values(this.Plugins).flatMap(Plugin => Plugin.AccountTypes)
+    }
+
+    GetAccountType(Id) {
+        return this.ListAccountTypes().find(AccountType => AccountType.Id == Id)
+    }
+}
+
+const CoreLauncher = new CoreLauncherClass()
+globalThis.CoreLauncher = CoreLauncher
+await WaitForEvent(document, "DOMContentLoaded")
+await CoreLauncher.AsyncLoad()
+await CoreLauncher.LoadPlugins()
+
+await CoreLauncher.ScreenManager.GetScreen("Main").Show()
+
+// document.addEventListener(
+//     "DOMContentLoaded",
+//     async function () {
+//         CoreLauncher.DataBase = new DataBaseClass(`${CoreLauncher.ApplicationData}/Database.json`)
+//         CoreLauncher.ScreenManager = new ScreenManagerClass()
+//         await (await Import("ga.corelauncher.Screens.Registry"))(CoreLauncher.ScreenManager)
+//         await (await Import("ga.corelauncher.Helpers.DeSVG"))()
+
+//         CoreLauncher.PluginManager = new PluginManagerClass(CoreLauncher.PluginsFolder)
+//         await CoreLauncher.PluginManager.LoadPlugins()
+//         CoreLauncher.AccountManager = new AccountManagerClass(CoreLauncher.PluginManager.ListAccountTypes())
+//         CoreLauncher.GameManager = new GameManagerClass(CoreLauncher.PluginManager.ListGames())
+//         CoreLauncher.TaskManager = new TaskManagerClass()
+//         CoreLauncher.WindowControl = new WindowControlClass()
+
+//         // await CoreLauncher.ScreenManager.GetScreen("Main").Show()
+//         await CoreLauncher.GameManager.Games[0].OpenSettings()
+
+//     }
+// )
