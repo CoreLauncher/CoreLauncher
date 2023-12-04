626c84e4a6a622bca44360c64e845dbe80d671cf
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-12-04 23:11:45 +0100

    load saved account instances

--- src/Main/js/ga/corelauncher/Classes/Extends/AccountType.js
+++ src/Main/js/ga/corelauncher/Classes/Extends/AccountType.js
@@ -1,5 +1,28 @@
+const ResourceBase64 = await Import("ga.corelauncher.Helpers.ResourceBase64")
+
 class AccountType {
-    
+    constructor(Type, AccountInstanceClass) {
+
+        const AccountsData = Object.values(CoreLauncher.DataBase.GetKey("Accounts")).filter((Account) => Account.Type == Type)
+        console.log(AccountsData)
+        const AccountInstances = AccountsData.map(
+            (AccountData) => {
+                const AccountInstance = new AccountInstanceClass()
+                AccountInstance.FromSavedData(AccountData.Data)
+                AccountInstance.Type = Type
+                AccountInstance.InstanceUUID = AccountData.UUID
+                AccountInstance.ConnectedAt = AccountData.ConnectedAt
+                return AccountInstance
+            }
+        )
+        console.log(AccountInstances)
+        this.AccountInstances = AccountInstances
+        console.log(this)
+    }
+
+    GetIconBase64() {
+        return ResourceBase64(this.Icon)
+    }
 }
 
 return AccountType
