1875c90e5ca3d79129879d2ff05fa1aff27d1878
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-12-05 23:20:27 +0100

    add accounts list

--- src/Main/js/ga/corelauncher/Screens/Main/Settings/Accounts/Main.js
+++ src/Main/js/ga/corelauncher/Screens/Main/Settings/Accounts/Main.js
@@ -1,12 +1,83 @@
+let AccountTemplate
+
+function LoadAccounts(ScreenElement, Accounts) {
+    const AccountsList = ScreenElement.querySelector(".instanceslist")
+    AccountsList.innerHTML = ""
+
+    for (const Account of Accounts) {
+
+        const AccountElement = AccountTemplate.cloneNode(true)
+        AccountElement.querySelector(".name").innerText = Account.GetName()
+        AccountElement.querySelector(".id").innerText = Account.GetId()
+        AccountElement.querySelector("img").src = Account.GetIcon()
+        AccountsList.appendChild(AccountElement)
+    }
+}
+
 return {
     Default: false,
 
     Init: async function(Screen, ScreenElement, ScreenManager, Data) {
+        const AccountSelector = ScreenElement.querySelector(".accountselector select")
+        const AccountFilter = ScreenElement.querySelector(".listfilter select")
+        const StartConnectionButton = ScreenElement.querySelector(".accountselector button")
+
+        StartConnectionButton.addEventListener(
+            "click",
+            async () => {
+                const AccountType = CoreLauncher.GetAccountType(AccountSelector.value)
+                await AccountType.StartConnection()
+            }
+        )
+
+        AccountTemplate = ScreenElement.querySelector(".account")
+        AccountTemplate.remove()
 
     },
 
     ApplyShowStyle: true,
     Show: async function(Screen, ScreenElement, ScreenManager, Data) {
+        const AccountTypes = CoreLauncher.ListAccountTypes()
+
+        const AccountSelectorImage = ScreenElement.querySelector(".accountselector img")
+        const AccountSelector = ScreenElement.querySelector(".accountselector select")
+
+        const AccountFilterImage = ScreenElement.querySelector(".listfilter img")
+        const AccountFilter = ScreenElement.querySelector(".listfilter select")
+
+        AccountSelector.innerHTML = ""
+        AccountFilter.innerHTML = ""
+
+        const AllOption = document.createElement("option")
+        AllOption.value = "all"
+        AllOption.innerText = "All account types"
+        AccountFilter.appendChild(AllOption)
+        
+        for (const AccountType of AccountTypes) {
+            const AccountSelectorOption = document.createElement("option")
+            AccountSelectorOption.value = AccountType.Type
+            AccountSelectorOption.innerText = AccountType.Name
+            AccountSelector.appendChild(AccountSelectorOption)
+
+            const AccountFilterOption = document.createElement("option")
+            AccountFilterOption.value = AccountType.Type
+            AccountFilterOption.innerText = AccountType.Name
+            AccountFilter.appendChild(AccountFilterOption)
+        }
+
+        function OnChangeFn(Img, Select) {
+            return function() {
+                if (Select.value == "all") {Img.src = ""; return}
+                Img.src = CoreLauncher.GetAccountType(Select.value).GetIconBase64()
+            }
+        }
+
+        AccountSelector.addEventListener("changed", OnChangeFn(AccountSelectorImage, AccountSelector))
+        OnChangeFn(AccountSelectorImage, AccountSelector)()
+        AccountFilter.addEventListener("changed", OnChangeFn(AccountFilterImage, AccountFilter))
+        OnChangeFn(AccountFilterImage, AccountFilter)()
+
+        LoadAccounts(ScreenElement, CoreLauncher.ListAccountInstances())
 
     },
     
