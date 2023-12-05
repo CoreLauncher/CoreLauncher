3f2d350b8dc571bcdf545c3b3a74d31095a67f25
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-12-05 23:19:00 +0100

    add save func

--- src/Main/js/ga/corelauncher/Classes/Extends/AccountInstance.js
+++ src/Main/js/ga/corelauncher/Classes/Extends/AccountInstance.js
@@ -1,5 +1,20 @@
 class AccountInstance {
-    
+    constructor() {
+
+    }
+
+    Save() {
+        const SaveData = this.SaveData()
+        CoreLauncher.DataBase.SetKey(
+            `Accounts.${this.InstanceUUID}`,
+            {
+                Type: this.Type,
+                UUID: this.InstanceUUID,
+                ConnectedAt: this.ConnectedAt,
+                Data: SaveData
+            }
+        )
+    }
 }
 
 return AccountInstance
