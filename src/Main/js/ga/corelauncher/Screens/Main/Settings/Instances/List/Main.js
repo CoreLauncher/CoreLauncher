d8cfc7ff37818a88e0fca7fc442b664b656276b1
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-12-11 22:26:09 +0100

    add instance list

--- src/Main/js/ga/corelauncher/Screens/Main/Settings/Instances/List/Main.js
+++ src/Main/js/ga/corelauncher/Screens/Main/Settings/Instances/List/Main.js
@@ -1,13 +1,30 @@
+let InstanceTemplate
+
 return {
     Default: false,
 
     Init: async function(Screen, ScreenElement, ScreenManager, Data) {
         ScreenElement.querySelector(".createbutton").addEventListener("click", () => { Screen.ParentScreen.GetScreen("Create").Show(Screen.Data) })
+
+        InstanceTemplate = ScreenElement.querySelector(".instancelist .instance")
+        InstanceTemplate.remove()
     },
 
     ApplyShowStyle: true,
     Show: async function(Screen, ScreenElement, ScreenManager, Game) {
         ScreenElement.querySelector(".title").innerText = `${Game.Name} instances`
+
+        const Instances = Game.ListInstances()
+
+        const InstanceListElement = ScreenElement.querySelector(".instancelist")
+        InstanceListElement.innerHTML = ""
+
+        for (const Instance of Instances) {
+            const InstanceElement = InstanceTemplate.cloneNode(true)
+            InstanceElement.querySelector(".name").innerText = Instance.GetName()
+            
+            InstanceListElement.appendChild(InstanceElement)
+        }
     },
     
     ApplyHideStyle: true,
