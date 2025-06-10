904cc1ea300bfc85389572dc99315ab96ed4d5f0
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2025-06-11 00:35:53 +0200

    prevent context menu

--- plugins/plugin-react-frontend/public/App.tsx
+++ plugins/plugin-react-frontend/public/App.tsx
@@ -1,6 +1,18 @@
+import { useEffect } from "react";
 import "./index.css";
 
 export function App() {
+	useEffect(() => {
+		function onContextMenu(event: MouseEvent) {
+			event.preventDefault();
+		}
+
+		document.addEventListener("contextmenu", onContextMenu);
+		return () => {
+			document.removeEventListener("contextmenu", onContextMenu);
+		};
+	}, []);
+
 	return <div className="App">Hi</div>;
 }
 
