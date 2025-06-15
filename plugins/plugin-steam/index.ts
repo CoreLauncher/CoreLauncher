abb30ea3a6ed2be9308c6f61ea5c1f91da4d5e20
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2025-06-16 01:19:00 +0200

    Update index.ts

--- plugins/plugin-steam/index.ts
+++ plugins/plugin-steam/index.ts
@@ -1,4 +1,7 @@
-import type { PluginShape } from "@corelauncher/types";
+import { PluginShape } from "@corelauncher/types";
+import { getSteamInstallationDirectory } from "./util/registry";
+
+async function noop() {}
 
 export const id = "plugin-steam";
 export const format = 1;
@@ -6,6 +9,15 @@ export const name = "Steam";
 export const description =
 	"Allows you to launch Steam games from CoreLauncher.";
 
-export class Plugin implements PluginShape {
-	constructor() {}
+export class Plugin extends PluginShape implements PluginShape {
+	constructor() {
+		super();
+
+		// this.steamDirectory =
+		// console.log(this.steamDirectory);
+
+		noop().then(async () => {
+			console.log(await getSteamInstallationDirectory());
+		});
+	}
 }
