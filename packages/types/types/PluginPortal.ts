43da9c96cd32d039f4d43fed0ba45ecf962561f6
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2025-06-20 19:36:41 +0200

    Update PluginPortal.ts

--- packages/types/types/PluginPortal.ts
+++ packages/types/types/PluginPortal.ts
@@ -1,5 +1,12 @@
 import type { GameShape } from "../shapes/GameShape";
 
+/**
+ * The PluginPortal class is a plugins way to access the resources from other plugins.
+ */
 export abstract class PluginPortal {
+	/**
+	 * Returns a list of games from all plugins.
+	 * @returns {GameShape[]} An array of GameShape objects.
+	 */
 	abstract getGames(): GameShape[];
 }
