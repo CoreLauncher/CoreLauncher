2c8ebc77cdc4412c389722a053e3c087cb441568
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2025-06-22 00:16:36 +0200

    add searchbar

--- plugins/plugin-react-frontend/public/sections/GameList/GameList.tsx
+++ plugins/plugin-react-frontend/public/sections/GameList/GameList.tsx
@@ -9,6 +9,7 @@ type Game = {
 
 export default function GameList() {
 	const [games, setGames] = useState<Game[]>([]);
+	const [query, setQuery] = useState("");
 
 	useEffect(() => {
 		fetchGames().then((data) => {
@@ -16,13 +17,26 @@ export default function GameList() {
 		});
 	}, []);
 
+	function onQuery(event: React.ChangeEvent<HTMLInputElement>) {
+		setQuery(event.target.value);
+	}
+
 	return (
 		<div className="GameList">
-			{games.map((game) => (
-				<div className="game" key={game.id}>
-					<p>{game.name}</p>
-				</div>
-			))}
+			<input type="text" placeholder="Search..." onChange={onQuery} />
+			<div className="games">
+				{games
+					.filter(
+						(game) =>
+							game.name.toLowerCase().includes(query.toLowerCase()) ||
+							game.id.toLowerCase().includes(query.toLowerCase()),
+					)
+					.map((game) => (
+						<div className="game" key={game.id}>
+							<p>{game.name}</p>
+						</div>
+					))}
+			</div>
 		</div>
 	);
 }
