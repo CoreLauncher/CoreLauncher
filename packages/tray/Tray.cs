8b8e6a4398e1d39093a468ceed8d74f9e38652c3
jens <jensmonne@gmail.com>
2025-06-22 22:11:53 +0200

    tray stuff

---
+++ packages/tray/Tray.cs
@@ -0,0 +1,42 @@
+﻿using System;
+#if WINDOWS
+using System.Windows.Forms;
+#endif
+namespace CoreLauncher;
+
+public class Tray : IDisposable
+{
+#if WINDOWS
+    private NotifyIcon notifyIcon;
+#endif
+    
+    public event EventHandler? Click;
+
+    public Tray(string iconPath, string tooltip = "")
+    {
+#if WINDOWS
+        notifyIcon = new NotifyIcon();
+        notifyIcon.Icon = new System.Drawing.Icon(iconPath);
+        notifyIcon.Text = tooltip;
+        notifyIcon.Visible = true;
+        notifyIcon.Click += (sender, args) => Click?.Invoke(this, EventArgs.Empty);
+#endif
+        
+        // TODO: add macOS and Linux implementations
+    }
+    
+    public void Dispose()
+    {
+        Destroy();
+    }
+
+    public void Destroy()
+    { 
+#if WINDOWS
+        NotifyIcon.Visible = false;
+        NotifyIcon.Dispose();
+        notifyIcon = null;
+#endif
+        // TODO: macOS and Linux cleanup
+    }
+}
---
+++ packages/tray/TrayExport.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace CoreLauncher;
+
+public static class TrayExport
+{
+    private static Tray? tray;
+
+    [UnmanagedCallersOnly(EntryPoint = "tray_create")]
+    public static void CreateTray()
+    {
+        tray = new Tray("icon.icon", "My Application");
+        tray.Click += (_, _) => Console.WriteLine("Tray Clicked");
+    }
+
+    [UnmanagedCallersOnly(EntryPoint = "tray_destroy")]
+    public static void DestroyTray()
+    {
+        tray?.Dispose();
+        tray = null;
+    }
+}
