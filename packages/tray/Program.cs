5305c52d15eea4cb8711a7df1de1dfd4fe2fa8e3
jens <jensmonne@gmail.com>
2025-06-22 19:04:50 +0200

    start on tray

--- packages/tray/Program.cs
+++ packages/tray/Program.cs
@@ -1,1 +1,41 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+#if WINDOWS
+using System.Windows.Forms;
+#endif
+namespace CoreLauncher;
+
+public class Tray : IDisposable
+{
+#if WINDOWS
+    private NotifyIcon notifyIcon;
+#endif
+    
+    public event EventHandler? Click;
+
+    public Tray(string iconPath, string tooltip = "")
+    {
+#if WINDOWS
+        notifyIcon = new NotifyIcon();
+        notifyIcon.Icon = new System.Drawing.Icon(iconPath);
+        notifyIcon.Text = tooltip;
+        notifyIcon.Visible = true;
+        notifyIcon.Click += (sender, args) => Click?.Invoke(this, EventArgs.Empty);
+#endif
+        
+        // TODO: add macOS and Linux implementations
+    }
+    
+    public void Dispose()
+    {
+        Destroy();
+    }
+
+    public void Destroy()
+    { 
+#if WINDOWS
+NotifyIcon.Visible = false;
+NotifyIcon.Dispose();
+#endif
+        // TODO: macOS and Linux cleanup
+    }
+}
