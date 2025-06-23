75f8eeb1d9443b3a599d8f9dfc410fb2921a2c7b
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2025-06-23 23:30:16 +0200

    Various improvements to Tray TS code

--- packages/tray/index.ts
+++ packages/tray/index.ts
@@ -1,9 +1,7 @@
 import { dlopen, FFIType, JSCallback, ptr } from "bun:ffi";
 import dll from "./tray.dll" with { type: "file" };
-
-function encodeCString(value: string) {
-	return ptr(new TextEncoder().encode(`${value}\0`));
-}
+import { TypedEmitter } from "tiny-typed-emitter";
+import { existsSync } from "node:fs";
 
 const lib = dlopen(dll, {
 	tray_create: {
@@ -20,29 +18,38 @@ const lib = dlopen(dll, {
 	},
 });
 
-export class Tray {
-	private clickCallback?: JSCallback;
+function encodeCString(value: string) {
+	return ptr(new TextEncoder().encode(`${value}\0`));
+}
+
+interface TrayEvents {
+	click: () => void;
+}
+
+export class Tray extends TypedEmitter<TrayEvents> {
 	public created = false;
 
-	onClick(callback: () => void) {
-		if (!this.created) throw new Error("Tray not created yet");
-		if (this.clickCallback) this.clickCallback.close();
+	create(name: string, icon: string) {
+		if (this.created) throw new Error("Tray already created");
+		if (!existsSync(icon)) throw new Error(`Icon file does not exist: ${icon}`);
 
-		this.clickCallback = new JSCallback(callback, {
-			args: [],
-			returns: FFIType.void,
-		});
-
-		if (!this.clickCallback.ptr) throw new Error("Callback pointer is null");
-		lib.symbols.tray_register_click_callback(this.clickCallback.ptr);
-	}
-
-	create(iconPath: string, name: string) {
-		if (this.created) throw new Error("Tray already created!");
-		const encodedPath = encodeCString(iconPath);
 		const encodedName = encodeCString(name);
+		const encodedIcon = encodeCString(icon);
 
-		lib.symbols.tray_create(encodedPath, encodedName);
+		const onClick = new JSCallback(
+			() => {
+				console.log("Tray icon clicked!");
+				this.emit("click");
+			},
+			{
+				args: [],
+				returns: FFIType.void,
+			},
+		);
+
+		if (!onClick.ptr) throw new Error("Callback pointer is null");
+		lib.symbols.tray_register_click_callback(onClick.ptr);
+		lib.symbols.tray_create(encodedIcon, encodedName);
 		this.created = true;
 	}
 
