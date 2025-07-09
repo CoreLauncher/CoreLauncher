e60e27be47dc69aa27bb5c3dff74644efab0d617
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2025-07-09 14:16:39 +0200

    add file to dataurl package

---
+++ packages/file-to-dataurl/index.ts
@@ -0,0 +1,12 @@
+import { readFileSync } from "node:fs";
+
+export function fileToDataURL(path: string) {
+	const file = Bun.file(path);
+	const content = readFileSync(path, "utf-8");
+
+	return dataToDataURL(content, file.type);
+}
+
+export function dataToDataURL(data: string, type: string) {
+	return `data:${type};base64,${Buffer.from(data).toString("base64")}`;
+}
