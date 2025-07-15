461bd2f1ff2aad39efd853b3d9ff3baef3af8d4a
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2025-07-15 14:15:53 +0200

    add docs

--- packages/types/shapes/GameShape.ts
+++ packages/types/shapes/GameShape.ts
@@ -2,5 +2,9 @@ export abstract class GameShape {
 	abstract id: string;
 	abstract name: string;
 
+	/**
+	 * Launches the game.
+	 * @returns {boolean | string | Promise<boolean | string>} A boolean indicating success or a string failure message.
+	 */
 	abstract launch(): boolean | string | Promise<boolean | string>;
 }
