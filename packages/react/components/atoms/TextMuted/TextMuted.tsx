c86236253ec786b6be2c1217a5d707d82fe4204a
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2025-07-24 22:39:57 +0200

    add textmuted component

---
+++ packages/react/components/atoms/TextMuted/TextMuted.tsx
@@ -0,0 +1,5 @@
+import "./TextMuted.css";
+
+export function TextMuted({ children }: { children: string }) {
+	return <p className="cl-text-muted">{children}</p>;
+}
--- packages/react/index.ts
+++ packages/react/index.ts
@@ -1,3 +1,5 @@
 export * from "./components/atoms/Button/Button";
 export * from "./components/atoms/Input/Input";
+export * from "./components/atoms/Logo/Logo";
+export * from "./components/atoms/TextMuted/TextMuted";
 export * from "./components/other/Style/Style";
