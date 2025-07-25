8757c6fe6ef6ad01dbfd81cd81cc1481a70d15ec
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2025-07-25 17:13:30 +0200

    update appdata directory

--- projects/corelauncher/util/directories.ts
+++ projects/corelauncher/util/directories.ts
@@ -4,13 +4,13 @@ import { existsSync } from "fs-extra";
 
 export function applicationDirectory() {
 	if (process.env.CORELAUNCHER_APP_DIR) return process.env.CORELAUNCHER_APP_DIR;
-	if (!isProduction) return join(process.cwd(), ".data");
+	if (!isProduction) return join(process.cwd(), ".CoreLauncher");
 	if (process.platform === "win32") {
 		const appdata = process.env.APPDATA || "";
 		if (!existsSync(appdata))
 			throw new Error("APPDATA environment variable is not set or invalid.");
 
-		return join(appdata, "CoreLauncher");
+		return join(appdata, ".CoreLauncher");
 	}
 
 	throw new Error("No valid application directory found.");
