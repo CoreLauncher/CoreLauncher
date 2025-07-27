73ab41484d48ed7239f1cfb203302281b6d0f9ec
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2025-07-27 23:03:15 +0200

    remove commented out code

--- projects/corelauncher/classes/PluginManager.ts
+++ projects/corelauncher/classes/PluginManager.ts
@@ -2,23 +2,6 @@ import type { PluginExport } from "@corelauncher/types";
 import { TypedEmitter } from "tiny-typed-emitter";
 import PluginContainer from "./PluginContainer";
 
-// type Plugin = {
-// 	id: string;
-// 	name: string;
-// 	description: string;
-// 	Plugin: new (
-// 		...args: ConstructorParameters<typeof PluginShape>
-// 	) => PluginShape;
-// };
-
-// type LoadedPlugin = Plugin & {
-// 	constructed?: InstanceType<typeof PluginShape>;
-// 	ready: boolean;
-// 	parts: {
-// 		games: InstanceType<typeof GameShape>[];
-// 	};
-// };
-
 interface PluginManagerEvents {
 	"plugin-ready": (plugin: PluginContainer) => void;
 }
@@ -51,36 +34,6 @@ export default class PluginManager extends TypedEmitter<PluginManagerEvents> {
 			);
 			this.emit("plugin-ready", container);
 		});
-
-		// const portal = new PluginPortal(this);
-		// const constructed = new plugin.Plugin(portal);
-		// const loaded = {
-		// 	...plugin,
-		// 	constructed: constructed,
-		// 	portal,
-		// 	ready: false,
-		// 	parts: {
-		// 		games: [],
-		// 	},
-		// } as LoadedPlugin;
-
-		// constructed.on("games", (games: InstanceType<typeof GameShape>[]) => {
-		// 	if (loaded.ready)
-		// 		throw new Error(
-		// 			"Can't update games after ready event has been emitted.",
-		// 		);
-		// 	loaded.parts.games = games;
-		// });
-
-		// constructed.on("ready", () => {
-		// 	console.info(
-		// 		`Plugin "${plugin.name}" (${plugin.id}) loaded successfully.`,
-		// 	);
-		// 	loaded.ready = true;
-		// 	this.emit("plugin-ready", loaded);
-		// });
-
-		// this.plugins.push(loaded);
 	}
 
 	// async enablePlugin(id: string) {}
