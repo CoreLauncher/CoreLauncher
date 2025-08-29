6d6bcb4c7b284a5d8566b7f7d5eceb93260f0902
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2025-08-29 23:09:26 +0200

    change corelauncher exe and folders to lowercase

--- projects/corelauncher/util/directories.ts
+++ projects/corelauncher/util/directories.ts
@@ -4,13 +4,13 @@ import { existsSync } from "fs-extra";
 
 export function applicationDirectory() {
 	if (process.env.CORELAUNCHER_APP_DIR) return process.env.CORELAUNCHER_APP_DIR;
-	if (!isProduction) return join(process.cwd(), ".CoreLauncher");
+	if (!isProduction) return join(process.cwd(), ".corelauncher");
 	if (process.platform === "win32") {
 		const appdata = process.env.APPDATA || "";
 		if (!existsSync(appdata))
 			throw new Error("APPDATA environment variable is not set or invalid.");
 
-		return join(appdata, ".CoreLauncher");
+		return join(appdata, ".corelauncher");
 	}
 
 	throw new Error("No valid application directory found.");
