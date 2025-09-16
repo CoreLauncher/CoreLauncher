9fd4ce12ec7cdc47e5538f55a15ed70718fabf97
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2025-09-16 11:43:45 +0200

    work on release action

--- packages/hash-block/bin.ts
+++ packages/hash-block/bin.ts
@@ -1,13 +1,28 @@
+import "@corelauncher/console-addon";
+
 import { existsSync } from "node:fs";
 import { join } from "node:path";
+import { argv } from "bun";
 import { encodeHashes, generateHashes } from ".";
 
-const file = Bun.argv[2];
-if (!file) throw new Error("No file patht provided");
-const resolved = join(process.cwd(), file);
+const inputFile = argv[2];
+const outputFile = argv[3];
+
+console.info("Input file:", inputFile);
+console.info("Output file:", outputFile);
+
+if (!inputFile) throw new Error("No input file path provided");
+if (!outputFile) throw new Error("No output file path provided");
+
+const resolved = join(process.cwd(), inputFile);
 if (!existsSync(resolved))
 	throw new Error(`File path does not exist: ${resolved}`);
 
+console.info(`Generating hashes...`);
+
 const hashes = await generateHashes(resolved);
 const encoded = await encodeHashes(hashes);
-console.log(encoded);
+
+console.info(`Writing ${hashes.length} hashes to ${outputFile}`);
+await Bun.write(outputFile, encoded);
+console.info("Done!");
