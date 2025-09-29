8f6d604a02eb87a0911cff37ac1c41d0ce21d7a6
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2025-09-29 23:59:43 +0200

    add a fast loading animation

--- plugins/plugin-react-frontend/public/app/App.tsx
+++ plugins/plugin-react-frontend/public/app/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { Style } from "@corelauncher/react";
 import { useState } from "react";
 import LibraryPage from "../components/Pages/LibraryPage/LibraryPage";
+import LoadingPage from "../components/Pages/LoadingPage/LibraryPage";
 import SettingsPage from "../components/Pages/SettingsPage/SettingsPage";
 import Header from "../components/Sections/Header/Header";
 import useDisableContextMenu from "../hooks/useDisableContextMenu";
@@ -20,6 +21,7 @@ export default function App() {
 				/>
 				<LibraryPage isVisible={page === "library"} />
 				<SettingsPage isVisible={page === "settings"} />
+				<LoadingPage />
 			</div>
 		</Style>
 	);
