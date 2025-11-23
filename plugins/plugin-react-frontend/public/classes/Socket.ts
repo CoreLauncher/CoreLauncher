a9f1daf0765f89a33cacc98776d6bb8129bbb84d
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2025-11-23 19:57:59 +0100

    reconnect socket

--- plugins/plugin-react-frontend/public/classes/Socket.ts
+++ plugins/plugin-react-frontend/public/classes/Socket.ts
@@ -10,11 +10,19 @@ interface EventsEvents {
 }
 
 export default class Socket extends TypedEmitter<EventsEvents> {
+	static get instance() {
+		if (!Socket._instance) Socket._instance = new Socket();
+		return Socket._instance;
+	}
+
 	private static _instance: Socket | null = null;
-	private ws: WebSocket;
+	private ws: WebSocket | null = null;
 	constructor() {
 		super();
+		this.connect();
+	}
 
+	private connect() {
 		this.ws = new WebSocket("/socket");
 		this.ws.addEventListener("message", (event) => {
 			const message = JSON.parse(event.data);
@@ -23,14 +31,10 @@ export default class Socket extends TypedEmitter<EventsEvents> {
 		});
 
 		this.ws.addEventListener("open", () => console.info("Socket connected"));
-		this.ws.addEventListener("close", () =>
-			console.info("Socket disconnected"),
-		);
-	}
-
-	static get instance() {
-		if (!Socket._instance) Socket._instance = new Socket();
-		return Socket._instance;
+		this.ws.addEventListener("close", () => {
+			console.info("Socket disconnected");
+			setTimeout(() => this.connect(), 1000);
+		});
 	}
 
 	/**
@@ -41,6 +45,7 @@ export default class Socket extends TypedEmitter<EventsEvents> {
 	send<Type extends MessageType>(type: Type, data: MessageTypeMap[Type]) {
 		console.info(`Sending message ${type}:`, data);
 		const stringified = JSON.stringify({ type, data });
+		if (!this.ws) throw new Error("WebSocket is not connected");
 		this.ws.send(stringified);
 	}
 }
