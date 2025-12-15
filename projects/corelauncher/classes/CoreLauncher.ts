5d22564bc648f9b161aab0f2d1a6efcb5df29473
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2025-12-15 11:00:04 +0100

    add database

--- projects/corelauncher/classes/CoreLauncher.ts
+++ projects/corelauncher/classes/CoreLauncher.ts
@@ -1,7 +1,11 @@
+import { join } from "node:path";
+import createDatabase from "@corelauncher/database";
 import * as EpicPlugin from "@corelauncher/plugin-epic-games";
 import * as ReactPlugin from "@corelauncher/plugin-react-frontend";
 import * as SteamPlugin from "@corelauncher/plugin-steam";
-
+import type { Kysely } from "kysely";
+import type { Database } from "../types/database";
+import { applicationDirectory } from "../util/directories";
 import PluginManager from "./PluginManager";
 import SingleInstanceLock from "./SingleInstanceLock";
 
@@ -9,11 +13,18 @@ import SingleInstanceLock from "./SingleInstanceLock";
  * CoreLauncher class that initializes and manages plugins.
  */
 export default class CoreLauncher {
+	database: Promise<Kysely<Database>>;
+
 	singleInstanceLock: SingleInstanceLock;
 	plugins: PluginManager;
 	constructor() {
 		this.singleInstanceLock = new SingleInstanceLock();
 		this.plugins = new PluginManager();
+
+		this.database = createDatabase<Database>(
+			join(applicationDirectory(), "corelauncher.sqlite"),
+			[],
+		);
 
 		this.plugins.loadPlugin(ReactPlugin);
 		this.plugins.loadPlugin(SteamPlugin);
---
+++ projects/corelauncher/types/database.ts
@@ -0,0 +1,2 @@
+// biome-ignore lint/complexity/noBannedTypes: Temporary
+export type Database = {};
