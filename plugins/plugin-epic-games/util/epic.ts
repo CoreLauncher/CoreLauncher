38fd58d3970339db54ef2d1f89c2dc06d56ee453
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2025-12-17 20:49:49 +0100

    remove log

--- plugins/plugin-epic-games/util/epic.ts
+++ plugins/plugin-epic-games/util/epic.ts
@@ -80,7 +80,6 @@ export async function getEpicGames() {
 		const content = await readFile(filePath, "utf-8");
 
 		const manifest = JSON.parse(content) as EpicManifest;
-		console.log(manifest);
 
 		games.push({
 			id: manifest.AppName,
