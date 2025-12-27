0f5f5cf20613d41bb44e6136b7c2ffb3a5a30543
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2025-12-27 15:35:10 +0100

    return to library if pressed again #62

--- plugins/plugin-react-frontend/public/app/App.tsx
+++ plugins/plugin-react-frontend/public/app/App.tsx
@@ -1,24 +1,35 @@
 import "./App.css";
 import { Style } from "@corelauncher/react";
-import { Activity, useState } from "react";
+import { Activity, useRef, useState } from "react";
 import Header from "../components/Sections/Header/Header";
 import useDisableContextMenu from "../hooks/useDisableContextMenu";
-import LibraryPage from "../pages/LibraryPage/LibraryPage";
+import LibraryPage, {
+	type LibraryPageRefObject,
+} from "../pages/LibraryPage/LibraryPage";
 import LoadingPage from "../pages/LoadingPage/LibraryPage";
 import ProfilePage from "../pages/ProfilePage/ProfilePage";
 import SettingsPage from "../pages/SettingsPage/SettingsPage";
 
 export default function App() {
+	const libraryRef = useRef<LibraryPageRefObject>(null);
 	const [page, setPage] = useState("library");
 	useDisableContextMenu();
+
+	function onSelect(tab: string) {
+		if (page !== tab) {
+			setPage(tab);
+		} else if (tab === "library") {
+			libraryRef.current?.clearSelection();
+		}
+	}
 
 	return (
 		<Style>
 			<div className="App">
-				<Header tab={page} onSelect={(tab) => setPage(tab)} />
+				<Header tab={page} onSelect={onSelect} />
 
 				<Activity mode={page === "library" ? "visible" : "hidden"}>
-					<LibraryPage />
+					<LibraryPage ref={libraryRef} />
 				</Activity>
 
 				<Activity mode={page === "profile" ? "visible" : "hidden"}>
--- plugins/plugin-react-frontend/public/pages/LibraryPage/LibraryPage.tsx
+++ plugins/plugin-react-frontend/public/pages/LibraryPage/LibraryPage.tsx
@@ -1,11 +1,25 @@
 import "./LibraryPage.css";
-import { useState } from "react";
+import { type Ref, useImperativeHandle, useState } from "react";
 import LibraryList from "../../components/Sections/LibraryList/LibraryList";
 import LibraryOverview from "../../components/Sections/LibraryOverview/LibraryOverview";
 import GameView from "./subcompontents/GameView/GameView";
 
-export default function LibraryPage() {
+export interface LibraryPageRefObject {
+	clearSelection: () => void;
+}
+
+export default function LibraryPage({
+	ref,
+}: {
+	ref: Ref<LibraryPageRefObject>;
+}) {
 	const [selected, setSelected] = useState<string | null>(null);
+
+	useImperativeHandle(ref, () => {
+		return {
+			clearSelection: () => setSelected(null),
+		};
+	}, []);
 
 	return (
 		<main className="LibraryPage">
