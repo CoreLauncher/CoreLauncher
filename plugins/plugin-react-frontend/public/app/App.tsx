872fb68ed9bf272e28ce751af6ea26c9b86f9633
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2025-12-27 01:52:52 +0100

    add placeholder profile page

--- plugins/plugin-react-frontend/public/app/App.tsx
+++ plugins/plugin-react-frontend/public/app/App.tsx
@@ -5,6 +5,7 @@ import Header from "../components/Sections/Header/Header";
 import useDisableContextMenu from "../hooks/useDisableContextMenu";
 import LibraryPage from "../pages/LibraryPage/LibraryPage";
 import LoadingPage from "../pages/LoadingPage/LibraryPage";
+import ProfilePage from "../pages/ProfilePage/ProfilePage";
 import SettingsPage from "../pages/SettingsPage/SettingsPage";
 
 export default function App() {
@@ -18,6 +19,10 @@ export default function App() {
 
 				<Activity mode={page === "library" ? "visible" : "hidden"}>
 					<LibraryPage />
+				</Activity>
+
+				<Activity mode={page === "profile" ? "visible" : "hidden"}>
+					<ProfilePage />
 				</Activity>
 
 				<Activity mode={page === "settings" ? "visible" : "hidden"}>
---
+++ plugins/plugin-react-frontend/public/pages/ProfilePage/ProfilePage.tsx
@@ -0,0 +1,5 @@
+import "./ProfilePage.css";
+
+export default function ProfilePage() {
+	return <main className="ProfilePage">Nothing to see here... yet.</main>;
+}
