dd39af91b554937802b983e07f12d99ee8ce4744
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2025-12-28 16:40:32 +0100

    cleanup

--- scripts/patch-webview.ts
+++
@@ -1,9 +0,0 @@
-import { join } from "node:path";
-
-const flag = "//@ts-nocheck";
-const file = join("node_modules", "webview-bun", "src", "ffi.ts");
-const content = await Bun.file(file).text();
-
-if (content.startsWith(flag)) process.exit(0);
-const patchedContent = `${flag}\n${content}`;
-await Bun.write(file, patchedContent);
