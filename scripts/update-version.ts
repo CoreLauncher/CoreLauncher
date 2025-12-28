c66d636e99c8eb1760c8ac305724cd376a9191e8
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2025-12-28 20:17:06 +0100

    remove v from version string

--- scripts/update-version.ts
+++ scripts/update-version.ts
@@ -1,8 +1,8 @@
 const packageJSONFile = Bun.file("./package.json");
-const version = process.argv[2];
+const version = process.argv[2]!;
 const packageJSON = await packageJSONFile.json();
 console.log(version, packageJSON);
 
-packageJSON.version = version;
+packageJSON.version = version.startsWith("v") ? version.slice(1) : version;
 
 Bun.write(packageJSONFile, `${JSON.stringify(packageJSON, null, 2)}\n`);
