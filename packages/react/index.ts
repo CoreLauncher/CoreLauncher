3b2ed2dc3752f0ad6e81f88f38b2c4ba4fc7ded2
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2026-01-17 00:59:25 +0100

    export new components

--- packages/react/index.ts
+++ packages/react/index.ts
@@ -1,9 +1,20 @@
+// Atoms
 export * from "./components/atoms/Block/Block";
-export * from "./components/atoms/Button/Button";
 export * from "./components/atoms/CopyRight/CopyRight";
-export * from "./components/atoms/Input/Input";
 export * from "./components/atoms/Logo/Logo";
-export * from "./components/atoms/TextMuted/TextMuted";
+
+// Input
+export * from "./components/input/Button/Button";
+export * from "./components/input/TextInput/TextInput";
+
+// Molecules
 export * from "./components/molecules/Dialog/Dialog";
 export * from "./components/molecules/WindowControls/WindowControls";
+
+// Other
 export * from "./components/other/Style/Style";
+
+// Text
+export * from "./components/text/Link/Link";
+export * from "./components/text/Text/Text";
+export * from "./components/text/TextMuted/TextMuted";
