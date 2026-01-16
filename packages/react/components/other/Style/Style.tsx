b91318f9d5034f231f20b5ea39382fe0913986f5
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2026-01-16 16:10:08 +0100

    allow setting the background to transparent

--- packages/react/components/other/Style/Style.tsx
+++ packages/react/components/other/Style/Style.tsx
@@ -3,6 +3,26 @@ import "../../../style/index.css";
 /**
  * This component loads all the styles used by CoreLauncher.
  */
-export function Style({ children }: { children?: React.ReactNode }) {
-	return children;
+export function Style({
+	children,
+	hasTransparentBackground = false,
+}: {
+	children?: React.ReactNode;
+	hasTransparentBackground?: boolean;
+}) {
+	return (
+		<>
+			{hasTransparentBackground && (
+				<style>
+					{`
+					html, body, #root {
+						background:transparent;
+					}
+					`}
+				</style>
+			)}
+			{children}
+		</>
+	);
+	// return children;
 }
