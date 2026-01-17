47bcbd93d95343ce5883a6370cd88953816b80b3
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2026-01-17 01:39:08 +0100

    Update ProfilePage.tsx

--- plugins/plugin-react-frontend/public/pages/ProfilePage/ProfilePage.tsx
+++ plugins/plugin-react-frontend/public/pages/ProfilePage/ProfilePage.tsx
@@ -1,5 +1,9 @@
 import "./ProfilePage.css";
 
 export default function ProfilePage() {
-	return <main className="ProfilePage">Nothing to see here... yet.</main>;
+	return (
+		<main className="ProfilePage">
+			Nothing to see here... <i>yet</i>.
+		</main>
+	);
 }
