4fe65b4207918c7c1e69edf278b7fb6379d52be9
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2026-01-20 21:24:40 +0100

    color hr

--- plugins/plugin-react-frontend/public/pages/LibraryPage/subcompontents/PlayBar/PlayBar.tsx
+++ plugins/plugin-react-frontend/public/pages/LibraryPage/subcompontents/PlayBar/PlayBar.tsx
@@ -1,8 +1,13 @@
 import { type Icon, PlayFill, Plus } from "react-bootstrap-icons";
 import "./PlayBar.css";
 import { GameType } from "@corelauncher/sdk";
-import { MessageType } from "../../../../../types/messages";
+import { useState } from "react";
+import {
+	type GamesUpdatedMessage,
+	MessageType,
+} from "../../../../../types/messages";
 import Socket from "../../../../classes/Socket";
+import CreateInstanceDialog from "../../../../dialogs/CreateInstanceDialog/CreateInstanceDialog";
 import { useGameStore } from "../../../../stores/GameStore";
 
 export default function PlayBar({
@@ -12,18 +17,26 @@ export default function PlayBar({
 	meta?: { icon: Icon; title: string; content: string }[];
 	gameId: string;
 }) {
+	const [isCreatingInstance, setIsCreatingInstance] = useState(false);
 	const game = useGameStore((state) => state.getGame(gameId));
 	if (!game) throw new Error(`Game with ID ${gameId} not found`);
 
-	function onPlay() {
-		Socket.instance.send(MessageType.LaunchGame, {
-			id: gameId,
-		});
+	function onPlay(game: GamesUpdatedMessage["games"][0]) {
+		switch (game.type) {
+			case GameType.Normal:
+				return Socket.instance.send(MessageType.LaunchGame, {
+					id: gameId,
+				});
+			case GameType.Instanced: {
+				setIsCreatingInstance(true);
+				break;
+			}
+		}
 	}
 
 	return (
 		<div className="PlayBar">
-			<button className="playbutton" type="button" onClick={onPlay}>
+			<button className="playbutton" type="button" onClick={() => onPlay(game)}>
 				{game.type === GameType.Normal && <PlayFill size={"90%"} />}
 				{game.type === GameType.Instanced && <Plus size={"90%"} />}
 			</button>
@@ -38,6 +51,12 @@ export default function PlayBar({
 					</div>
 				))}
 			</div>
+			{isCreatingInstance && (
+				<CreateInstanceDialog
+					gameId={gameId}
+					onClose={() => setIsCreatingInstance(false)}
+				/>
+			)}
 		</div>
 	);
 }
