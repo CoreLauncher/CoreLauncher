49c06c9b07cebe08ec5b41bcb6eeae90e0b000c1
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2026-02-06 15:36:26 +0100

    change window bounds

--- crates/corelauncher_app/src/main.rs
+++ crates/corelauncher_app/src/main.rs
@@ -41,7 +41,7 @@ fn main() {
     Application::new().run(|cx: &mut App| {
         gpui_component::init(cx);
 
-        let bounds = Bounds::centered(None, size(px(500.), px(500.0)), cx);
+        let bounds = Bounds::centered(None, size(px(1200.), px(800.0)), cx);
         cx.open_window(
             WindowOptions {
                 window_bounds: Some(WindowBounds::Windowed(bounds)),
