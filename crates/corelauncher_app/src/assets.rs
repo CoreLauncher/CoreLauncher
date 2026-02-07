20daac5557f8012029e418bb37029cdb7f6508ca
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2026-02-07 15:32:43 +0100

    add assets

---
+++ crates/corelauncher_app/src/assets.rs
@@ -0,0 +1,26 @@
+use std::borrow::Cow;
+
+use anyhow::Result;
+use gpui::{AssetSource, SharedString};
+
+#[derive(rust_embed::RustEmbed)]
+#[folder = "../../assets"]
+pub struct CustomAssets;
+
+impl AssetSource for CustomAssets {
+    fn load(&self, path: &str) -> Result<Option<Cow<'static, [u8]>>> {
+        if path.is_empty() {
+            return Ok(None);
+        }
+
+        Self::get(path)
+            .map(|f| Some(f.data))
+            .ok_or_else(|| panic!("could not find asset at path \"{path}\""))
+    }
+
+    fn list(&self, path: &str) -> Result<Vec<SharedString>> {
+        Ok(Self::iter()
+            .filter_map(|p| p.starts_with(path).then(|| p.into()))
+            .collect())
+    }
+}
