db8cb2beff32ed7df9a425dfd39925e3cec9f6db
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2026-02-12 21:47:17 +0100

    finish buildscript to download protos

--- crates/steam-client/build.rs
+++ crates/steam-client/build.rs
@@ -1,5 +1,3 @@
-// use protobuf_codegen::CodeGen;
-use reqwest::Client;
 use serde::{Deserialize, Serialize};
 use std::{env, fs, path::PathBuf};
 
@@ -10,8 +8,8 @@ struct GithubFile {
     download_url: String,
 }
 
-async fn fetch_directory(
-    client: Client,
+fn fetch_directory(
+    client: &reqwest::blocking::Client,
     owner: &str,
     repo: &str,
     commit: &str,
@@ -22,58 +20,77 @@ async fn fetch_directory(
         owner, repo, directory, commit
     );
 
-    let response = client.get(contents_url).send().await;
-    let text = response.unwrap().text().await.unwrap();
-    println!("{:?}", text);
-    let files: Vec<GithubFile> = serde_json::from_str(&text).unwrap();
-    println!("{:?}", files);
+    let response = client.get(contents_url).send().unwrap();
+    let files = response.json().unwrap();
     return files;
 }
 
-async fn download_file(client: Client, file: &GithubFile) {
-    let response = client.get(&file.download_url).send().await;
-    let content = response.unwrap().bytes().await.unwrap();
-    let path = PathBuf::from(format!("protobuf/{}", file.path));
-    if let Some(parent) = path.parent() {
+fn download_file(client: &reqwest::blocking::Client, destination: &PathBuf, file: &GithubFile) {
+    let response = client.get(&file.download_url).send();
+    let content = response.unwrap().bytes().unwrap();
+    if let Some(parent) = destination.parent() {
         fs::create_dir_all(parent).unwrap();
     }
-    fs::write(path, content).unwrap();
+    fs::write(destination, content).unwrap();
 }
 
-async fn download_protos() {
+fn download_protos(out_dir: &PathBuf, client: &reqwest::blocking::Client) -> Vec<PathBuf> {
     let owner = "SteamDatabase";
     let repo = "Protobufs";
     let commit = "f98bd7fe1d3c2f4fab269e5b31a37638bcf99760";
     let directories = ["steam", "google/protobuf", "webui"];
+    let protos_dir = out_dir.join("protos");
+    let mut protos: Vec<PathBuf> = Vec::new();
 
-    let client = reqwest::Client::builder()
+    for directory in directories {
+        let files = fetch_directory(client, owner, repo, commit, directory);
+        for file in files {
+            let path = protos_dir.join(&file.path);
+            download_file(&client, &path, &file);
+            if file.path.starts_with("steam") {
+                protos.push(path);
+            }
+        }
+    }
+
+    return protos;
+}
+
+fn download_protoc(out_dir: &PathBuf) {
+    let protoc_path = protoc_fetcher::protoc("31.1", out_dir).unwrap();
+    println!("{:?}", protoc_path);
+    unsafe { env::set_var("PROTOC", &protoc_path) };
+}
+
+fn main() {
+    println!("cargo::rerun-if-changed=build.rs");
+
+    let out_dir = env::var("OUT_DIR").map(|path| PathBuf::from(path)).unwrap();
+    let client = reqwest::blocking::Client::builder()
         .user_agent("CoreLauncher")
         .build()
         .unwrap();
 
-    for directory in directories {
-        let files = fetch_directory(client.clone(), owner, repo, commit, directory).await;
+    download_protoc(&out_dir);
+    let protos = download_protos(&out_dir, &client);
 
-        for file in files {
-            download_file(client.clone(), &file).await;
+    let result = prost_build::compile_protos(
+        protos.as_slice(),
+        &[
+            out_dir.join("protos/steam"),
+            out_dir.join("protos/google/protobuf"),
+            out_dir.join("protos/webui"),
+        ],
+    );
+
+    match result {
+        Ok(_) => {
+            println!("Successfully compiled protos.");
+        }
+        Err(e) => {
+            panic!("Failed to compile protos: {}", e);
         }
     }
-}
 
-#[tokio::main]
-async fn main() {
-    let cwd = env::current_dir().unwrap();
-    let _ = fs::create_dir(cwd.join("protobuf"));
-
-    download_protos().await;
-
-    // CodeGen::new()
-    //     .include("protobuf")
-    //     .dependency(protobuf_well_known_types::get_dependency(
-    //         "protobuf_well_known_types",
-    //     ))
-    //     .generate_and_compile()
-    //     .unwrap();
-
-    panic!("Logs")
+    panic!("Hi")
 }
