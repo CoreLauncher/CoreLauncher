bca9d1216e19eec0be6ada3aac4ef51e51d25df5
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2026-07-05 15:47:44 +0200

    add window dragging

--- packages/corelauncher-app/public/components/sections/header/header.tsx
+++ packages/corelauncher-app/public/components/sections/header/header.tsx
@@ -2,8 +2,16 @@ import BrandingLogo from "../../branding/branding-logo/branding-logo";
 import "./header.css";
 
 export default function Header() {
+    function onMouseDown(event: React.MouseEvent) {
+		const target = event.target as HTMLElement;
+		if (target.nodeName !== "DIV" && target.nodeName !== "HEADER") return;
+        window.ipc.postMessage(JSON.stringify({
+            type: "window_drag"
+        }))
+	}
+
 	return (
-		<header className="Header">
+		<header className="Header" onMouseDown={onMouseDown}>
 			<BrandingLogo size={20} />
 			<p>Header</p>
 		</header>
--- packages/corelauncher-app/src/main.rs
+++ packages/corelauncher-app/src/main.rs
@@ -1,9 +1,10 @@
-use std::{borrow::Cow, fs};
+use std::{borrow::Cow, fs, thread};
 
 use crate::{assets::Assets, constants::Constants};
+use serde::Deserialize;
 use tao::{
-    dpi::{LogicalSize, Size},
-    event_loop::{ControlFlow, EventLoop},
+    dpi::LogicalSize,
+    event_loop::{ControlFlow, EventLoop, EventLoopBuilder},
     window::WindowBuilder,
 };
 use wry::{WebViewBuilder, http::Response};
@@ -12,22 +13,46 @@ mod assets;
 mod constants;
 mod plugins;
 
+#[derive(Debug, Deserialize)]
+#[serde(tag = "type", content = "payload")]
+#[serde(rename_all = "snake_case")]
+enum IPCEvent {
+    WindowDrag,
+}
+
+#[derive(Debug)]
+enum UserEvent {
+    IPCEvent(IPCEvent),
+}
+
 struct Window {
     window: tao::window::Window,
     webview: wry::WebView,
+    pub ipc_receiver: Option<std::sync::mpsc::Receiver<IPCEvent>>,
 }
 
 impl Window {
-    fn new(event_loop: &EventLoop<()>) -> Self {
+    fn new(event_loop: &EventLoop<UserEvent>) -> Self {
+        let (ipc_sender, ipc_receiver) = std::sync::mpsc::channel::<IPCEvent>();
+
         let window = WindowBuilder::new()
             .with_inner_size(LogicalSize::new(1200, 800))
             .with_decorations(false)
             .build(event_loop)
             .unwrap();
 
-        let webview_builder = WebViewBuilder::new().with_custom_protocol(
-            "corelauncher-webview".into(),
-            |_, request| {
+        let webview_builder = WebViewBuilder::new()
+            .with_ipc_handler(move |request| {
+                let body = request.body();
+                let data = serde_json::from_str::<IPCEvent>(body);
+
+                if let Ok(event) = data {
+                    ipc_sender.send(event).unwrap();
+                } else {
+                    eprintln!("Failed to parse IPC event: {:?}", data);
+                }
+            })
+            .with_custom_protocol("corelauncher-webview".into(), |_, request| {
                 let uri = request.uri().to_string();
                 let mut path = uri.split("://").last().unwrap_or("index.html");
 
@@ -52,8 +77,7 @@ impl Window {
                         .body(Cow::Owned("404 Not Found".into()))
                         .unwrap();
                 }
-            },
-        );
+            });
 
         #[cfg(not(debug_assertions))]
         let webview_builder = webview_builder.with_url("corelauncher-webview://index.html");
@@ -77,7 +101,11 @@ impl Window {
 
         webview.open_devtools();
 
-        Self { window, webview }
+        Self {
+            window,
+            webview,
+            ipc_receiver: Some(ipc_receiver),
+        }
     }
 }
 
@@ -91,17 +119,40 @@ impl CoreLauncher {
     }
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     println!("App Directory: {:?}", Constants::app_directory());
     let _ = fs::create_dir(Constants::app_directory());
 
-    let event_loop = EventLoop::new();
+    let event_loop = EventLoopBuilder::with_user_event().build();
+    let event_proxy = event_loop.create_proxy();
 
     let mut app = CoreLauncher::new();
-    let window = Window::new(&event_loop);
+    let mut window = Window::new(&event_loop);
+    let ipc_receiver = window.ipc_receiver.take().unwrap();
+    thread::spawn(move || {
+        loop {
+            if let Ok(event) = ipc_receiver.recv() {
+                event_proxy.send_event(UserEvent::IPCEvent(event)).unwrap();
+            }
+        }
+    });
     app.main_window = Some(window);
 
-    event_loop.run(move |_, _, control_flow| {
+    event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Wait;
+
+        match event {
+            tao::event::Event::UserEvent(event) => {
+                println!("Received user event: {:#?}", event);
+                app.main_window
+                    .as_ref()
+                    .unwrap()
+                    .window
+                    .drag_window()
+                    .unwrap();
+            }
+            _ => {}
+        }
     });
 }
