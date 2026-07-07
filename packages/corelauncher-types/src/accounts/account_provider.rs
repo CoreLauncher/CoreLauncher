95d6bb502d1731d28ef0bd438274a36c14892a39
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2026-07-07 13:27:46 +0200

    serialize account provider id

--- packages/corelauncher-types/src/accounts/account_provider.rs
+++ packages/corelauncher-types/src/accounts/account_provider.rs
@@ -1,14 +1,16 @@
-use serde::Serialize;
+use serde::{Serialize, ser::SerializeStruct};
 
 pub trait AccountProvider: std::fmt::Debug + Send + Sync {
     fn id(&self) -> String;
 }
 
-impl Serialize for (dyn AccountProvider + Send + 'static) {
+impl Serialize for dyn AccountProvider + Send + 'static {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: serde::Serializer,
     {
-        serializer.serialize_str(&self.id())
+        let mut state = serializer.serialize_struct("AccountProvider", 1)?;
+        state.serialize_field("id", &self.id())?;
+        state.end()
     }
 }
